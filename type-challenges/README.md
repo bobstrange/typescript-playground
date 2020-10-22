35b2a6df8bce2b26b7f17c0efba1584e1f881c2c
bobstrange <bob.1983.g@gmail.com>
2020-10-22 22:29:40 +0900

    feat: Pick solution

--- type-challenges/README.md
+++ type-challenges/README.md
@@ -1,3 +1,8 @@
 # Type Challenges challenge
 
 My solutions for [Type<Challenge[]>](https://github.com/type-challenges/type-challenges)
+
+
+```shell
+npm run check
+```
---
+++ type-challenges/src/04_pick.ts
@@ -0,0 +1,28 @@
+import { Equal, Expect } from "@type-challenges/utils"
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+interface Expected1 {
+  title: string
+}
+
+interface Expected2 {
+  title: string
+  completed: boolean
+}
+
+type MyPick<T, K extends keyof T> = {
+  [k in K]: k extends keyof T ? T[k] : never
+}
+type Hoge = MyPick<Todo, "title" | "completed">
+
+type cases = [
+  Expect<Equal<Expected1, MyPick<Todo, "title">>>,
+  Expect<Equal<Expected2, MyPick<Todo, "title" | "completed">>>,
+  // @ts-expect-error
+  MyPick<Todo, "title" | "completed" | "invalid">
+]
