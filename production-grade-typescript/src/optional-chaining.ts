c62857b11aef274c5b43be3ed7f09972867cfe23
bobstrange <bob.1983.g@gmail.com>
2021-01-01 23:10:07 +0900

    feat: optional-chining example

--- production-grade-typescript/src/optional-chaining.ts
+++ production-grade-typescript/src/optional-chaining.ts
@@ -1,9 +1,28 @@
-let x: {
+const x: {
   user: {
     name: string
     address?: {
       street: string
       city: string
     }
+  }
+} = undefined as any
+
+// optional chaining
+x.user.address?.city
+
+Nullish - coalescing
+
+class Foo {
+  // # private field
+  #name: string
+  constructor(rawName?: string) {
+    // ?? と || の違い
+    // || は、rawName が '' や 0 などの falsy な値の場合に、右の値を返す
+    // ?? は、rwaName が null or undefined の場合の場合のみ、右の値を返す
+    this.#name = rawName ?? '(no name)'
+  }
+  log() {
+    console.log(this.#name)
   }
 }
