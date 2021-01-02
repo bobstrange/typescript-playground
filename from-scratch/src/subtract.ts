fee5ba8a93c4b5c329546c1831fb5ebdc80a27c1
bobstrange <bob.1983.g@gmail.com>
2021-01-02 23:46:11 +0900

    feat: added subtract and generate d.ts

---
+++ from-scratch/src/subtract.ts
@@ -0,0 +1,11 @@
+/**
+ * Calculate the difference between two number
+ *
+ * @param a - first number
+ * @param b - second number
+ *
+ * @beta
+ */
+export function subtract(a: number, b: number): number {
+  return a - b;
+}
