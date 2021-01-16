c45b3cf95b9314e1fde2bc325b04e8d9f1877607
bobstrange <bob.1983.g@gmail.com>
2021-01-16 23:35:46 +0900

    feat: make test pass

--- sls-mongo/jest.config.js
+++ sls-mongo/jest.config.js
@@ -1,11 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
     '^~(.*)$': '<rootDir>/src$1',
     '^~libs(.*)$': '<rootDir>/src/libs$1',
     '^~services(.*)$': '<rootDir>/src/services$1',
+  },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
 }
