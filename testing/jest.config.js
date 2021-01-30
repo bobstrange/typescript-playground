19f0a31a73ca8b46200670d57e95e197295c7d1d
bobstrange <bob.1983.g@gmail.com>
2021-01-31 00:25:48 +0900

    feat: output code coverage

--- testing/jest.config.js
+++ testing/jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-};
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
+}
