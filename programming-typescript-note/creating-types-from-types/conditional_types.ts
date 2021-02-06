3167c106fedded61c7bcaca43438b08d29d25b71
bobstrange <bob.1983.g@gmail.com>
2021-02-06 22:47:08 +0900

    feat: conditional types example

--- programming-typescript-note/creating-types-from-types/conditional_types.ts
+++ programming-typescript-note/creating-types-from-types/conditional_types.ts
@@ -32,3 +32,31 @@ type NameOrID<T extends number | string> = T extends number
 function refinedCreateLabel<T extends number | string>(input: T): NameOrID<T> {
   throw 'unimplemented'
 }
+
+// @ts-expect-error Type '"message"' cannot be used to index type T (T isn't known to have a property called 'message')
+type ErrorMessageOf<T> = T['message']
+
+// T の型を message という property を持つ型であると限定することによってエラーにならなくなる
+type MessageOf<T extends { message: unknown }> = T['message']
+
+interface IEmail {
+  message: string
+}
+
+interface IDog {
+  berk(): void
+}
+
+type EmailMessageContents = MessageOf<IEmail> // => string
+// @ts-expect-error IDog は message というプロパティを持たないので、エラーになる
+type ErrorDogMessageContents = MessageOf<IDog>
+
+// Messageof の型引数にどんな型でも渡したい場合は、ConditionalType を使えば良い
+type RefinedMessageOf<T> = T extends { message: unknown } ? T['message'] : never
+
+type EmailMessageContents2 = RefinedMessageOf<IEmail> // -> string
+type DogMessageContents = RefinedMessageOf<IDog> // -> never
+
+type Flatten<T> = T extends any[] ? T[number] : T
+type Str = Flatten<string[]> // -> type Str = string
+type Num = Flatten<number> // -> type Num = number
