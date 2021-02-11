47767ce85379a4d580cdf57c1237de4d6fc4639e
bobstrange <bob.1983.g@gmail.com>
2021-02-12 00:05:39 +0900

    feat: template literal memo

--- programming-typescript-note/creating-types-from-types/template_literal_types.ts
+++ programming-typescript-note/creating-types-from-types/template_literal_types.ts
@@ -1,2 +1,27 @@
 type World = 'world'
 type Greeting = `hello ${World}`
+
+type Horizontal = 'Left' | 'Right'
+type Vertical = 'Top' | 'Bottom'
+
+type Corner = `${Vertical}${Horizontal}`
+// type Corner = 'TopLeft' | 'TopRight' | 'BottomLeft' | 'BottomRight'
+
+type PropEventSource<Type> = {
+  on(eventName: `${string & keyof Type}Changed`): Promise<void>
+}
+
+declare function makeWatchedObject<Type>(
+  obj: Type
+): Type & PropEventSource<Type>
+
+const user = makeWatchedObject({
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 26,
+})
+
+user.on('firstNameChanged')
+
+// @ts-expect-error 間違えたキーを渡したときに型チェックに引っかかる
+user.on('firstName')
