b0a39c2e69284505faaaed141d635f5542c1cd51
bobstrange <bob.1983.g@gmail.com>
2021-02-12 23:14:37 +0900

    fix: fix lint configuration

--- testing-workshop/.eslintrc.js
+++ testing-workshop/.eslintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true
     },
     "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+      "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+        "prettier/@typescript-eslint"
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
