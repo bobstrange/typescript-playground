712e801359e13fd1a743388afc29a538499855ec
bobstrange <bob.1983.g@gmail.com>
2021-06-11 22:40:39 +0900

    feat: create link to notes

--- nextjs/getting-started/src/pages/index.tsx
+++ nextjs/getting-started/src/pages/index.tsx
@@ -1,5 +1,13 @@
 import React, { FC } from 'react'
+import Link from 'next/link'
 
-const IndexPage: FC = () => <div>Index Page</div>
+const IndexPage: FC = () => (
+  <div>
+    <h1>Index Page</h1>
+    <Link href="/notes">
+      <a>Notes</a>
+    </Link>
+  </div>
+)
 
 export default IndexPage
