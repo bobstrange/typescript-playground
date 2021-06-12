301dbf9a6c8e1eb06f4b1e32a5aa71cff5eaad6b
bobstrange <bob.1983.g@gmail.com>
2021-06-12 18:49:04 +0900

    feat: add theme file

--- nextjs/getting-started/src/pages/_app.tsx
+++ nextjs/getting-started/src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import { AppProps } from 'next/app'
+import { ThemeProvider } from 'theme-ui'
+
+import { theme } from '../theme'
 import '../styles/globals.css'
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 export default App
