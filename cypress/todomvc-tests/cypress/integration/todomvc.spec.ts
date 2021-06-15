7af77b1300c4f450180859f6df7bd1e4b89ef201
bobstrange <bob.1983.g@gmail.com>
2021-06-15 22:32:30 +0900

    refactor: seperate to 3 tests

--- cypress/todomvc-tests/cypress/integration/todomvc.spec.ts
+++ cypress/todomvc-tests/cypress/integration/todomvc.spec.ts
@@ -1,12 +1,17 @@
-it('should be able to add a new todo to the list', () => {
+it('should add a new todo to the list', () => {
   cy.visit('https://todomvc-app-for-testing.surge.sh')
   cy.get('.new-todo', { timeout: 6000 }).type('Tidy room{enter}')
   cy.get('label').should('have.text', 'Tidy room')
-
   cy.get('.toggle').should('not.be.checked')
+})
+
+it('should mark a todo as completed', () => {
   cy.get('.toggle').click()
   cy.get('.toggle').should('be.checked')
   cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
+})
+
+it('should clear completed todos', () => {
   cy.contains('Clear completed').click()
   cy.get('.todo-list').should('not.have.descendants', 'li')
 })
