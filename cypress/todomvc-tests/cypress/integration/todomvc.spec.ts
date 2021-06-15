696823faf96f7fe479db05affe0433192a8a22ed
bobstrange <bob.1983.g@gmail.com>
2021-06-15 10:04:32 +0900

    feat: add validations

--- cypress/todomvc-tests/cypress/integration/todomvc.spec.ts
+++ cypress/todomvc-tests/cypress/integration/todomvc.spec.ts
@@ -1,6 +1,11 @@
 it('should be able to add a new todo to the list', () => {
   cy.visit('https://todomvc-app-for-testing.surge.sh')
   cy.get('.new-todo', { timeout: 6000 }).type('Tidy room{enter}')
+  cy.get('label').should('have.text', 'Tidy room')
+
+  cy.get('.toggle').should('not.be.checked')
   cy.get('.toggle').click()
-  cy.contains('Clear completed').click()
+  cy.get('.toggle').should('be.checked')
+  cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
+  // cy.contains('Clear completed').click()
 })
