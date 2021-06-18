0fe0317488e2c2a42c1355926c508c85b5be630a
bobstrange <bob.1983.g@gmail.com>
2021-06-18 20:22:02 +0900

    refactor: refactor

--- cypress/todomvc-tests/cypress/integration/todomvc-filtering.spec.ts
+++ cypress/todomvc-tests/cypress/integration/todomvc-filtering.spec.ts
@@ -1,26 +1,29 @@
+import { TodoPage } from '../page-objects/todo-page'
+
 describe('todo filtering', () => {
+  const todoPage = new TodoPage()
+
   beforeEach(() => {
-    cy.visit('https://todomvc-app-for-testing.surge.sh')
+    todoPage.navigate()
+    todoPage.addTodo('Tidy room')
+    todoPage.addTodo('Learn cypress')
+    todoPage.addTodo('Buy some books')
 
-    cy.get('.new-todo').type('Tidy room{enter}')
-    cy.get('.new-todo').type('Learn cypress{enter}')
-    cy.get('.new-todo').type('Buy some books{enter}')
-
-    cy.get('.todo-list li:nth-child(2) .toggle').click()
+    todoPage.toggleTodo(2)
   })
 
   it('should filter "Active" todos', () => {
-    cy.contains('Active').click()
-    cy.get('.todo-list li').should('have.length', 2)
+    todoPage.showOnlyActiveTodos()
+    todoPage.validateNumberOfTodosShown(2)
   })
 
   it('should filter "Completed" todos', () => {
-    cy.contains('Completed').click()
-    cy.get('.todo-list li').should('have.length', 1)
+    todoPage.showOnlyCompleteTodos()
+    todoPage.validateNumberOfTodosShown(1)
   })
 
   it('should filter "All" todos', () => {
-    cy.contains('All').click()
-    cy.get('.todo-list li').should('have.length', 3)
+    todoPage.showAllTodos()
+    todoPage.validateNumberOfTodosShown(3)
   })
 })
