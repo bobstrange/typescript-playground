a8bc6b31588830b7866b3e8675579f58aea231c3
bobstrange <bob.1983.g@gmail.com>
2021-06-26 22:52:27 +0900

    feat: seeding data

--- nextjs/production-grade-nextjs/pages/api/auth/[...nextauth].ts
+++ nextjs/production-grade-nextjs/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import { connectToDB, doc, folder } from '../../../db'
 
 export default (req, res) =>
   NextAuth(req, res, {
@@ -18,5 +19,37 @@ export default (req, res) =>
     database: process.env.DATABASE_URL,
     pages: {
       signIn: '/signin',
+    },
+    callbacks: {
+      async session(session, user) {
+        if (user) {
+          session.user.id = user.id
+        }
+        return session
+      },
+      async jwt(tokenPayload, user, account, profile, isNewUser) {
+        const { db } = await connectToDB()
+        if (isNewUser) {
+          const newFolder = await folder.createFolder(db, { createdBy: user._id, name: 'Getting Started' })
+          await doc.createDoc(db, {
+            createdBy: user._id,
+            folder: newFolder._id,
+            name: 'Start Here',
+            content: {
+              time: 1624715229306,
+              blocks: [
+                {
+                  type: 'header',
+                  data: {
+                    text: 'Some default content',
+                    level: 2,
+                  },
+                },
+              ],
+              version: '3.1.4',
+            },
+          })
+        }
+      },
     },
   })
