c569fa00749762a815eb893374b42d79f3795c3c
bobstrange <bob.1983.g@gmail.com>
2021-07-04 17:19:56 +0900

    tmp

---
+++ nextjs/nextjs-blog/src/pages/posts/post.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+const Post = () => {
+  return (
+    <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  )
+}
+
+export default Post
