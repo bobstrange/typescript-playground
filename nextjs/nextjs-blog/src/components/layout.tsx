1d107e9cda7f997af38880e83926a00f1a4d5126
bobstrange <bob.1983.g@gmail.com>
2021-07-04 18:43:18 +0900

    feat: layout

--- nextjs/nextjs-blog/src/components/layout.tsx
+++ nextjs/nextjs-blog/src/components/layout.tsx
@@ -2,6 +2,11 @@
 
 import { FC } from 'react'
 import { css } from '@emotion/react'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import * as utilStyles from '../styles/utils'
 
 const container = css`
   max-width: 36em;
@@ -9,8 +14,79 @@ const container = css`
   margin: 3em auto 6em;
 `
 
-const Layout: FC = ({ children }) => {
-  return <div css={container}>{children}</div>
+const header = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const backToHome = css`
+  margin: 3em 0 0;
+`
+
+const name = 'bobstrange'
+export const siteTitle = 'Next.js Sample Website'
+
+const Layout: FC<{ home?: boolean }> = ({ children, home }) => {
+  return (
+    <div css={container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Learn how to build a personal website using Next.js" />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header css={header}>
+        {home ? (
+          <>
+            <Image
+              priority
+              src="/images/profile.png"
+              css={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt={name}
+            />
+            <h1 css={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <a>
+                <Image
+                  priority
+                  src="/images/profile.png"
+                  css={utilStyles.borderCircle}
+                  height={108}
+                  width={108}
+                  alt={name}
+                />
+              </a>
+            </Link>
+            <h2 css={utilStyles.headingLg}>
+              <Link href="/">
+                <a css={utilStyles.colorInherit}>{name}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div css={backToHome}>
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Layout
---
+++ nextjs/nextjs-blog/src/styles/utils.tsx
@@ -0,0 +1,55 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from '@emotion/react'
+
+export const heading2Xl = css`
+  font-size: 2.5rem;
+  line-height: 1.2;
+  font-weight: 800;
+  letter-spacing: -0.05rem;
+  margin: 1em 0;
+`
+export const headingXl = css`
+  font-size: 2rem;
+  line-height: 1.3;
+  font-weight: 800;
+  letter-spacing: -0.05rem;
+  margin: 1em 0;
+`
+
+export const headingLg = css`
+  font-size: 1.5rem;
+  line-height: 1.4;
+  margin: 1em 0;
+`
+
+export const headingMd = css`
+  font-size: 1.2rem;
+  line-height: 1.5;
+`
+
+export const borderCircle = css`
+  border-radius: 100em;
+`
+
+export const colorInherit = css`
+  color: inherit;
+`
+
+export const padding1px = css`
+  padding-top: 1px;
+`
+
+export const list = css`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`
+
+export const listItem = css`
+  margin: 0 0 1.25em;
+`
+
+export const lightText = css`
+  color: #666;
+`
