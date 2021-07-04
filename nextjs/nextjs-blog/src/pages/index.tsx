cf4bc531a214084b8e62a9459c91a7db7d94a211
bobstrange <bob.1983.g@gmail.com>
2021-07-04 19:17:44 +0900

    feat: getStaticProps example

--- nextjs/nextjs-blog/src/pages/index.tsx
+++ nextjs/nextjs-blog/src/pages/index.tsx
@@ -1,13 +1,28 @@
 /** @jsxImportSource @emotion/react */
 
 import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
 
 import * as utilStyles from '../styles/utils'
 import Layout, { siteTitle } from '../components/layout'
+import { GetStaticProps } from 'next'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export const getStaticProps: GetStaticProps<{ allPostsData: Post[] }> = async () => {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData,
+    },
+  }
+}
+
+type Post = {
+  date: string
+  title: string
+  id: string
+}
+
+export default function Home({ allPostsData }: { allPostsData: Post[] }) {
   return (
     <Layout home>
       <Head>
@@ -23,6 +38,20 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section css={{ ...utilStyles.headingMd, ...utilStyles.padding1px }}>
+        <h2 css={utilStyles.headingLg}>Blog</h2>
+        <ul css={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li key={id} css={utilStyles.listItem}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
