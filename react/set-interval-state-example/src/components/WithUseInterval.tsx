f9d2236e7b0cbfe618e963fcadc2fbeeb402ddd2
bobstrange <bob.1983.g@gmail.com>
2021-07-24 20:55:20 +0900

    feat: adjustable delay

--- react/set-interval-state-example/src/components/WithUseInterval.tsx
+++ react/set-interval-state-example/src/components/WithUseInterval.tsx
@@ -1,17 +1,33 @@
-import React, { FC, useState } from 'react'
+import React, {
+  ChangeEventHandler,
+  FC,
+  MouseEventHandler,
+  useState,
+} from 'react'
 import { useInterval } from '../composables/useInterval'
 
 export const WithUseInterval: FC = () => {
   const [count, setCount] = useState(0)
+  const [delay, setDelay] = useState(1000)
+
+  const handleCountReset: MouseEventHandler<HTMLButtonElement> = () => {
+    setCount(0)
+  }
+
+  const handleDelayChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setDelay(Number(e.target.value))
+  }
 
   useInterval(() => {
     setCount(count + 1)
-  }, 1000)
+  }, delay)
 
   return (
     <>
       <h2>With useInterval</h2>
       <div>Count: {count}</div>
+      <button onClick={handleCountReset}>Reset</button>
+      <input type="number" onChange={handleDelayChange} value={delay} />
     </>
   )
 }
