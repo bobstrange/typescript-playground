70e4eb99500ebaebd1aecd1802d3f50bb3c15d24
bobstrange <bob.1983.g@gmail.com>
2021-07-24 22:16:03 +0900

    chore: remove unused

--- react/timer/src/App.tsx
+++ react/timer/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 
 import { Timer } from './components/Timer'
-import { MyTimer } from './components/MyTimer'
+
 function App() {
   return (
     <div className="App">
       <Timer />
-      <MyTimer />
     </div>
   )
 }
