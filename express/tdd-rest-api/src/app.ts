56d73056d7081925032ce1ad06956aaa73fb72c3
bobstrange <bob.1983.g@gmail.com>
2021-07-25 23:06:00 +0900

    feat: add error handling

--- express/tdd-rest-api/src/app.ts
+++ express/tdd-rest-api/src/app.ts
@@ -1,6 +1,7 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import pinoHttp from 'pino-http'
 import cookieParser from 'cookie-parser'
+import createError, { HttpError } from 'http-errors'
 
 const app = express()
 const pino = pinoHttp()
@@ -12,6 +13,20 @@ app.use(cookieParser())
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello world!' })
+})
+
+app.use((req, res, next) => {
+  next(createError(404))
+})
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  res.status(err.status || 500)
+  res.json({
+    message: res.locals.message,
+    error: res.locals.error,
+  })
 })
 
 export { app }
