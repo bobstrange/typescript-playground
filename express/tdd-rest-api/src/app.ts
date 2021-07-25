395f1190133391cc39e56b1c739b02d78d746efe
bobstrange <bob.1983.g@gmail.com>
2021-07-25 22:45:48 +0900

    feat: add middlewares

--- express/tdd-rest-api/src/app.ts
+++ express/tdd-rest-api/src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express'
 import pinoHttp from 'pino-http'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 const pino = pinoHttp()
 
 app.use(pino)
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello world!' })
