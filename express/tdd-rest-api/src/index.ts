ab9521ecdf0f1880b2988d3428f8def723f6bda5
bobstrange <bob.1983.g@gmail.com>
2021-07-25 22:18:13 +0900

    refactor: move application related code to app

--- express/tdd-rest-api/src/index.ts
+++ express/tdd-rest-api/src/index.ts
@@ -1,10 +1,4 @@
-import express from 'express'
-
-const app = express()
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello world!' })
-})
+import { app } from './app'
 
 app.listen(3000, () => {
   console.log('listening port 3000')
