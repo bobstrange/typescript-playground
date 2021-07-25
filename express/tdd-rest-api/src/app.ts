96dbe5cdcff9027ba933de3945e313d2f4c82c7a
bobstrange <bob.1983.g@gmail.com>
2021-07-25 22:39:23 +0900

    feat: add pino for logger

--- express/tdd-rest-api/src/app.ts
+++ express/tdd-rest-api/src/app.ts
@@ -1,6 +1,10 @@
 import express from 'express'
+import pinoHttp from 'pino-http'
 
 const app = express()
+const pino = pinoHttp()
+
+app.use(pino)
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello world!' })
