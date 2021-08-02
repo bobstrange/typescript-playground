d8859a427226930cde130b91ec9068627c21f64e
bobstrange <bob.1983.g@gmail.com>
2021-08-02 22:21:58 +0900

    chore: map example

--- fundamentals/generics-example/src/index.ts
+++ fundamentals/generics-example/src/index.ts
@@ -1,7 +1,42 @@
-export type Dict = {};
+export type Dict<T> = {
+  [k: string]: T | undefined
+}
 
 // Array.prototype.map, but for Dict
-export function mapDict() {}
+export function mapDict<T, S>(
+  dict: Dict<T>,
+  fn: (arg: T, idx: number) => S
+): Dict<S> {
+  const out: Dict<S> = {}
+
+  Object.keys(dict).forEach((dictKey, idx) => {
+    const item = dict[dictKey]
+    if (typeof item !== 'undefined') {
+      out[dictKey] = fn(item, idx)
+    }
+  })
+
+  return out
+}
+
+mapDict(
+  {
+    a: 'a',
+    b: 'b',
+  },
+  (item, idx) => {
+    return [item]
+  }
+)
+mapDict(
+  {
+    a: 'a',
+    b: 'b',
+  },
+  (item, idx) => {
+    return { value: item, index: idx }
+  }
+)
 
 // Array.prototype.reduce, but for Dict
-export function reduceDict() {}
+export function reduceDict<T>(dict: Dict<T>) {}
