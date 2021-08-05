6621a8576d866f0f48b7cb06676f21ed618f5ffb
bobstrange <bob.1983.g@gmail.com>
2021-08-05 22:21:39 +0900

    chore: tidy

--- sls-mongo/src/types/env.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    readonly DB_USER?: string
-    readonly DB_PASSWORD?: string
-    readonly DB_HOST: string
-    readonly DB_PORT?: string
-    readonly DB_NAME?: string
-  }
-}
--- sls-mongo/tests/services/hello.service.spec.ts
+++
@@ -1,8 +0,0 @@
-import { fetchUsersByName } from '~/services/user.service'
-
-describe('hello.service', () => {
-  it('returns greeting message with given name', () => {
-    const res = fetchUsersByName('Bob')
-    expect(res).toEqual('Hello Bob, welcome to exciting serverless world')
-  })
-})
