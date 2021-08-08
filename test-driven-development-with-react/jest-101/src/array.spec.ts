b431f6a7e6e6480761543a9fe4f7b36a7d13c3bd
bobstrange <bob.1983.g@gmail.com>
2021-08-08 20:23:12 +0900

    feat: containing matcher

--- test-driven-development-with-react/jest-101/src/array.spec.ts
+++ test-driven-development-with-react/jest-101/src/array.spec.ts
@@ -1,9 +1,32 @@
-const users = ['John', 'Jane', 'James']
-
 it('match arrays', () => {
+  const users = ['John', 'Jane', 'James']
+
   // Strictly check (===)
   expect(users).toContain(users[0])
 
   // just checks the value
   expect(users).toContainEqual('John')
+})
+
+it('array containing', () => {
+  const users = ['John', 'Jane', 'James']
+
+  const userSet = expect.arrayContaining(['Jane', 'James'])
+  expect(users).toEqual(userSet)
+})
+
+it('object containing', () => {
+  const user = {
+    name: 'Bob Strange',
+    location: 'Tokyo',
+    hobby: [{ name: 'Programming' }, { name: 'Cycling' }],
+  }
+
+  // Can define matcher as you would in natural language
+  const matcher = expect.objectContaining({
+    name: expect.stringContaining('Bob'),
+    hobby: expect.arrayContaining([{ name: 'Programming' }]),
+  })
+
+  expect(user).toEqual(matcher)
 })
