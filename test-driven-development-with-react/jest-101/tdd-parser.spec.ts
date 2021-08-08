c940800e91aad8347a0df080c6f9c2af92e7ddeb
bobstrange <bob.1983.g@gmail.com>
2021-08-08 21:37:03 +0900

    feat: first test

---
+++ test-driven-development-with-react/jest-101/tdd-parser.spec.ts
@@ -0,0 +1,8 @@
+/**
+ *  parser for Tracking Progress
+ * ddBQbq -> { Dev days: 2.0, "QA days": 1, "Blocked days": 0.5 }
+ */
+
+it('translates d to half a dev day', () => {
+  expect(translate('d')).toEqual({ Dev: 0.5 })
+})
