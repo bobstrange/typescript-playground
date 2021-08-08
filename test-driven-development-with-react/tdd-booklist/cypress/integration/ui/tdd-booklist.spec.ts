42343fd8538c44877710d3ca79092722aec41de8
bobstrange <bob.1983.g@gmail.com>
2021-08-08 23:02:51 +0900

    feat: book list first acceptance test

--- test-driven-development-with-react/tdd-booklist/cypress/integration/ui/tdd-booklist.spec.ts
+++ test-driven-development-with-react/tdd-booklist/cypress/integration/ui/tdd-booklist.spec.ts
@@ -3,4 +3,10 @@ describe('TDD Booklist Application', () => {
     cy.visit('http://localhost:3000/')
     cy.get('[data-test=heading]').contains('TDD Booklist')
   })
+
+  it('Shows a book list', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('[data-test=book-list]')
+    cy.get('.book-item').should('have.length', 2)
+  })
 })
