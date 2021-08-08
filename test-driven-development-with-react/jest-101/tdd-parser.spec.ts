447dd4567b92ed49cd3c21957c0958517252404b
bobstrange <bob.1983.g@gmail.com>
2021-08-08 21:38:41 +0900

    feat: add a new test for triangulation

--- test-driven-development-with-react/jest-101/tdd-parser.spec.ts
+++ test-driven-development-with-react/jest-101/tdd-parser.spec.ts
@@ -8,3 +8,7 @@ const translate = (input: string) => ({ Dev: 0.5 })
 it('translates d to half a dev day', () => {
   expect(translate('d')).toEqual({ Dev: 0.5 })
 })
+
+it('translates D to one dev day', () => {
+  expect(translate('D')).toEqual({ Dev: 1 })
+})
