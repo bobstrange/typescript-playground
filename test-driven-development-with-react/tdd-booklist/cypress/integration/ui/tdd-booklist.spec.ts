75aa58c27c3780345d7c7ed9de917b94f3dc2390
bobstrange <bob.1983.g@gmail.com>
2021-08-08 23:21:33 +0900

    refactor: refactor renderer

--- test-driven-development-with-react/tdd-booklist/cypress/integration/ui/tdd-booklist.spec.ts
+++ test-driven-development-with-react/tdd-booklist/cypress/integration/ui/tdd-booklist.spec.ts
@@ -8,11 +8,11 @@ describe('TDD Booklist Application', () => {
     cy.visit('http://localhost:3000/')
     cy.get('[data-test=book-list]')
     cy.get('.book-item').should('have.length', 2)
-    cy.get('[data-test=book-list] > :nth-child(1)').should(
+    cy.get('[data-test=book-list] > :nth-child(1) > .title').should(
       'have.text',
       'Refactoring'
     )
-    cy.get('[data-test=book-list] > :nth-child(2)').should(
+    cy.get('[data-test=book-list] > :nth-child(2) > .title').should(
       'have.text',
       'Clean Code'
     )
--- test-driven-development-with-react/tdd-booklist/src/App.tsx
+++ test-driven-development-with-react/tdd-booklist/src/App.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import { Typography } from '@material-ui/core'
 
+type Book = {
+  name: string
+}
+
+const renderBooks = (books: Book[]): JSX.Element[] => {
+  return books.map((book) => (
+    <div className="book-item" key={book.name}>
+      <h2 className="title">{book.name}</h2>
+    </div>
+  ))
+}
+
 function App() {
+  const books = [{ name: 'Refactoring' }, { name: 'Clean Code' }]
+
   return (
     <div className="App">
       <Typography variant="h1" component="h1" data-test="heading">
         TDD Booklist
       </Typography>
-      <div data-test="book-list">
-        <div className="book-item">Refactoring</div>
-        <div className="book-item">Clean Code</div>
-      </div>
+      <div data-test="book-list">{renderBooks(books)}</div>
     </div>
   )
 }
