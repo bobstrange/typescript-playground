c2f01cdee22628441e2fe42b8e86def4315ff027
bobstrange <bob.1983.g@gmail.com>
2021-08-08 21:37:54 +0900

    feat: make the first test pass

--- test-driven-development-with-react/jest-101/tdd-parser.spec.ts
+++ test-driven-development-with-react/jest-101/tdd-parser.spec.ts
@@ -3,6 +3,8 @@
  * ddBQbq -> { Dev days: 2.0, "QA days": 1, "Blocked days": 0.5 }
  */
 
+const translate = (input: string) => ({ Dev: 0.5 })
+
 it('translates d to half a dev day', () => {
   expect(translate('d')).toEqual({ Dev: 0.5 })
 })
