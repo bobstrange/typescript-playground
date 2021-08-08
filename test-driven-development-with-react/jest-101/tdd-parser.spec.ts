cbcb53f57fd92f15b3e79e67e627cffcc797a695
bobstrange <bob.1983.g@gmail.com>
2021-08-08 21:39:39 +0900

    feat: make the test pass again

--- test-driven-development-with-react/jest-101/tdd-parser.spec.ts
+++ test-driven-development-with-react/jest-101/tdd-parser.spec.ts
@@ -3,7 +3,8 @@
  * ddBQbq -> { Dev days: 2.0, "QA days": 1, "Blocked days": 0.5 }
  */
 
-const translate = (input: string) => ({ Dev: 0.5 })
+const translate = (input: string) =>
+  input === 'd' ? { Dev: 0.5 } : { Dev: 1.0 }
 
 it('translates d to half a dev day', () => {
   expect(translate('d')).toEqual({ Dev: 0.5 })
