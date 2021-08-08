52b592b45a278008c4d08a1db7f865f041e5b285
bobstrange <bob.1983.g@gmail.com>
2021-08-08 21:44:28 +0900

    feat: make combination case dD test pass

--- test-driven-development-with-react/jest-101/tdd-parser.spec.ts
+++ test-driven-development-with-react/jest-101/tdd-parser.spec.ts
@@ -3,8 +3,20 @@
  * ddBQbq -> { Dev days: 2.0, "QA days": 1, "Blocked days": 0.5 }
  */
 
-const translate = (input: string) =>
-  input === 'd' ? { Dev: 0.5 } : { Dev: 1.0 }
+const translate = (input: string) => {
+  const sum = input
+    .split('')
+    .map((char) => {
+      return char === 'd' ? 0.5 : 1.0
+    })
+    .reduce((acc, curr) => {
+      return acc + curr
+    }, 0)
+
+  return {
+    Dev: sum,
+  }
+}
 
 it('translates d to half a dev day', () => {
   expect(translate('d')).toEqual({ Dev: 0.5 })
@@ -12,4 +24,8 @@ it('translates d to half a dev day', () => {
 
 it('translates D to one dev day', () => {
   expect(translate('D')).toEqual({ Dev: 1 })
+})
+
+it('translates dD to one and half dev days', () => {
+  expect(translate('dD')).toEqual({ Dev: 1.5 })
 })
