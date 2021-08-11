33d1ffcb8da7e24cbf6374f7ebcc2b400d864536
bobstrange <bob.1983.g@gmail.com>
2021-08-11 22:13:01 +0900

    feat: add BookDetail description test

--- test-driven-development-with-react/tdd-booklist/src/components/__tests__/BookDetail.spec.ts
+++ test-driven-development-with-react/tdd-booklist/src/components/__tests__/BookDetail.spec.ts
@@ -13,4 +13,18 @@ describe('BookDetail', () => {
     const title = container.querySelector('.book-title')
     expect(title.innerHTML).toEqual(props.book.name)
   })
+
+  it('renders description', () => {
+    const props = {
+      book: {
+        name: 'Refactoring',
+        description:
+          'Refactoring: Improving the Design of Existing Code, renowned object technology mentor Martin Fowler breaks new ground, demystifying these master practices and demonstrating how software practitioners can realize the significant benefits of this new process.',
+      },
+    }
+
+    const { container } = render(<BookDetail {...props} />)
+    const title = container.querySelector('.book-description')
+    expect(title.innerHTML).toEqual(props.book.description)
+  })
 })
