0ecd15fe296ca8752d43c11d51a98633dcd97633
bobstrange <bob.1983.g@gmail.com>
2021-08-14 23:12:00 +0900

    feature: don't send request when input is empty

--- test-driven-development-with-react/tdd-booklist/src/components/SearchBox.tsx
+++ test-driven-development-with-react/tdd-booklist/src/components/SearchBox.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react'
 import { TextField } from '@material-ui/core'
+import clone from 'lodash.clone'
+import isEmpty from 'lodash.isempty'
 
 type Props = {
   word: string
@@ -7,12 +9,21 @@ type Props = {
 }
 
 export const SearchBox: FC<Props> = ({ word, onSearch }) => {
+  const protectedOnSearch: React.ChangeEventHandler<
+    HTMLTextAreaElement | HTMLInputElement
+  > = (e) => {
+    const value = clone(e.target.value)
+    if (!isEmpty(value.trim())) {
+      onSearch(e)
+    }
+  }
+
   return (
     <TextField
       label="Search"
       value={word}
       data-test="search"
-      onChange={onSearch}
+      onChange={protectedOnSearch}
       margin="normal"
       variant="outlined"
     />
