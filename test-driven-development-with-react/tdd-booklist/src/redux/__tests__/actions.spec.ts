cbbbe2c22653d08fce9e9c96cb4596130cfa5674
bobstrange <bob.1983.g@gmail.com>
2021-08-15 22:35:48 +0900

    feat: add fetch books store test

--- test-driven-development-with-react/tdd-booklist/src/redux/__tests__/actions.spec.ts
+++ test-driven-development-with-react/tdd-booklist/src/redux/__tests__/actions.spec.ts
@@ -1,4 +1,11 @@
-import { setSearchWord } from '../actions'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import axios from 'axios'
+
+import { setSearchWord, fetchBooks } from '../actions'
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
 
 describe('BookListContainer related actions', () => {
   it('Sets the search keyword', () => {
@@ -8,6 +15,25 @@ describe('BookListContainer related actions', () => {
       word,
     }
     const action = setSearchWord(word)
-    expect(actoun).toEqual(expected)
+    expect(action).toEqual(expected)
+  })
+
+  it('Fetches data successfully', async () => {
+    const books = [
+      { name: 'Refactoring', id: 1 },
+      { name: 'Clean Code', id: 2 },
+      { name: 'Clean Architecture', id: 3 },
+    ]
+    axios.get = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve({ data: books }))
+
+    const expectedActions = [
+      { type: 'FETCH_BOOKS_PENDING' },
+      { type: 'FETCH_BOOKS_SUCCESS', books },
+    ]
+    const store = mockStore({ books: [] })
+    await store.dispatch(fetchBooks(''))
+    expect(store.getActions()).toEqual(expectedActions)
   })
 })
