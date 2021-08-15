c306d51a662263fcf30db738883472f589863cdb
bobstrange <bob.1983.g@gmail.com>
2021-08-15 22:53:47 +0900

    feat: add book search action test

--- test-driven-development-with-react/tdd-booklist/src/redux/__tests__/actions.spec.ts
+++ test-driven-development-with-react/tdd-booklist/src/redux/__tests__/actions.spec.ts
@@ -52,4 +52,21 @@ describe('BookListContainer related actions', () => {
     await store.dispatch(fetchBooks())
     expect(store.getActions()).toEqual(expectedActions)
   })
+
+  it('Search data with word', async () => {
+    const books = [
+      { name: 'Refactoring', id: 1 },
+      { name: 'Clean Code', id: 2 },
+      { name: 'Clean Architecture', id: 3 },
+    ]
+    axios.get = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve({ data: books }))
+
+    const store = mockStore({ books: [] })
+    await store.dispatch(fetchBooks('Refactoring'))
+    expect(axios.get).toHaveBeenCalledWith(
+      'http://localhost:8080/books?q=Refactoring'
+    )
+  })
 })
