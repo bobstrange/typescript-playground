ca6ad8df35c13344440ad591c6ed01e772eb617e
bobstrange <bob.1983.g@gmail.com>
2021-08-23 22:12:56 +0900

    feat: just returns an auction

--- serverless/auction-service/src/functions/createAuction/handler.ts
+++ serverless/auction-service/src/functions/createAuction/handler.ts
@@ -9,10 +9,19 @@ import schema from './schema'
 const createAuction: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
   event
 ) => {
-  return formatJSONResponse({
-    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
-    event,
-  })
+  const { title } = event.body
+  const auction = {
+    title,
+    status: 'OPEN',
+    createdAt: new Date().toISOString(),
+  }
+
+  return formatJSONResponse(
+    {
+      auction,
+    },
+    201
+  )
 }
 
 export const main = middyfy(createAuction)
