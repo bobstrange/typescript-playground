827f13412b84f1a998faee64bfedc405fccd8587
bobstrange <bob.1983.g@gmail.com>
2021-08-27 22:43:17 +0900

    feat: add http-error middleware

--- serverless/auction-service/src/libs/commonMiddleware.ts
+++ serverless/auction-service/src/libs/commonMiddleware.ts
@@ -1,6 +1,7 @@
 import middy from '@middy/core'
 import httpJsonBodyParser from '@middy/http-json-body-parser'
+import httpErrorHandler from '@middy/http-error-handler'
 
 export const commonMiddleware = (handler) => {
-  return middy(handler).use([httpJsonBodyParser()])
+  return middy(handler).use([httpJsonBodyParser(), httpErrorHandler()])
 }
