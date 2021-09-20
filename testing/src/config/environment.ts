71c2acd84519847db7c4d12cd41f9fd8499909cd
bobstrange <bob.1983.g@gmail.com>
2021-09-20 22:50:57 +0900

    tidy

--- testing/src/config/environment.ts
+++
@@ -1,3 +0,0 @@
-const port = parseInt(process.env.PORT || '8080')
-
-export const PORT = port
--- testing/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { app } from './app'
-import { PORT } from './config/environment'
-
-app.listen(PORT, () => {
-  console.log('Listening on port: ', PORT)
-})
