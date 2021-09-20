acf68d0b4db8b267274c4cafd4449717c4559482
bobstrange <bob.1983.g@gmail.com>
2021-09-20 23:09:45 +0900

    feat: add XHR testing example

--- e2e-cypress/cypress/integration/firsttest.spec.ts
+++ e2e-cypress/cypress/integration/firsttest.spec.ts
@@ -1,5 +1,14 @@
 describe('Complete code base of E2E test', () => {
-  it('Launching EA website', () => {
+  beforeEach(() => {
+    cy.intercept('/api/data-bundle/home-page').as('homepage')
     cy.visit('https://www.executeautomation.com')
+  })
+
+  it('Launching EA website', () => {})
+
+  it('XHR Testing', () => {
+    cy.wait('@homepage')
+      .its('response.statusCode')
+      .should('be.oneOf', [200, 304])
   })
 })
