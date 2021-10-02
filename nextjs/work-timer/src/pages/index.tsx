a0dacfc3ae8afef232223e627d6b1832611ed855
bobstrange <bob.1983.g@gmail.com>
2021-10-02 22:59:01 +0900

    feat: add Timer

--- nextjs/work-timer/src/pages/index.tsx
+++ nextjs/work-timer/src/pages/index.tsx
@@ -1,47 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
+
+import { Timer } from '../components/Timer'
 
 export default function Home() {
-  const [hour, setHour] = useState(0)
-  const [minute, setMinute] = useState(0)
-
-  const changeHandler = (
-    setter: React.Dispatch<React.SetStateAction<number>>
-  ) => {
-    return (e) => {
-      console.log(e.target.value)
-      setter(parseInt(e.target.value))
-    }
-  }
-
+  const time = new Date()
+  time.setSeconds(time.getSeconds() + 600) // 10 minutes timer
   return (
-    <>
-      <form action="">
-        <input
-          type="number"
-          name="hour"
-          id="hour"
-          placeholder="h"
-          value={hour ? hour : ''}
-          autoComplete="off"
-          spellCheck="false"
-          onChange={changeHandler(setHour)}
-          min="0"
-          size={1}
-        />
-        <input
-          type="number"
-          name="minute"
-          id="minute"
-          placeholder="m"
-          value={minute ? minute : ''}
-          autoComplete="off"
-          spellCheck="false"
-          onChange={changeHandler(setMinute)}
-          min="0"
-          size={1}
-        />
-        <button type="submit">Start</button>
-      </form>
-    </>
+    <div>
+      <Timer expiryTimestamp={time} />
+    </div>
   )
 }
