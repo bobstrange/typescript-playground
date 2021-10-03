e890626e129bce915d4b4f25f43b9dfb4ed46fe2
bobstrange <bob.1983.g@gmail.com>
2021-10-03 23:42:45 +0900

    update context

--- nextjs/pomodoro/src/context/SettingsContext.tsx
+++ nextjs/pomodoro/src/context/SettingsContext.tsx
@@ -11,13 +11,21 @@ export type Props = {
   stopTimer?: () => void
   updateExecute?: (arg: PomodoroSettings) => void
   setTimerTime?: (arg: PomodoroSettings) => void
+  pomodoro?: number
+  executing?: PomodoroSettings | Record<string, never>
+  startAnimate?: boolean
+  startTimer?: () => void
+  pauseTimer?: () => void
+  setCurrentTimer?: (arg: PomodoroSettings['active']) => void
+  remaining?: ({ remainingTimer }: { remainingTimer: number }) => string
+  settingButton?: () => void
 }
 
 export const SettingContext = createContext<Props>({})
 
 export const SettingsContextProvider: React.FC = ({ children }) => {
   const [pomodoro, setPomodoro] = useState(0)
-  const [executing, setExecuting] = useState({})
+  const [executing, setExecuting] = useState<PomodoroSettings>({})
   const [startAnimate, setStartAnimate] = useState(false)
 
   function startTimer() {
@@ -34,6 +42,16 @@ export const SettingsContextProvider: React.FC = ({ children }) => {
 
   function updateExecute(updatedSettings: PomodoroSettings) {
     setExecuting(updatedSettings)
+  }
+
+  function settingButton() {
+    setExecuting({})
+    setPomodoro(0)
+  }
+
+  function setCurrentTimer(activeState: PomodoroSettings['active']) {
+    updateExecute({ ...executing, active: activeState })
+    setTimerTime(executing)
   }
 
   function setTimerTime(setting: PomodoroSettings) {
@@ -53,8 +71,28 @@ export const SettingsContextProvider: React.FC = ({ children }) => {
     }
   }
 
+  const remaining = ({ remainingTimer }: { remainingTimer: number }) => {
+    const minutes = Math.floor(remainingTimer / 60)
+    const seconds = remainingTimer % 60
+    return `${minutes}:${seconds}`
+  }
+
   return (
-    <SettingContext.Provider value={{ stopTimer, updateExecute, setTimerTime }}>
+    <SettingContext.Provider
+      value={{
+        stopTimer,
+        updateExecute,
+        setTimerTime,
+        executing,
+        pomodoro,
+        startAnimate,
+        startTimer,
+        pauseTimer,
+        remaining,
+        setCurrentTimer,
+        settingButton,
+      }}
+    >
       {children}
     </SettingContext.Provider>
   )
