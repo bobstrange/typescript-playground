605aeed2b68d20f6a1ebb29e1f9a9d518005473a
bobstrange <bob.1983.g@gmail.com>
2021-10-17 17:08:45 +0900

    feat: add Seo component

---
+++ gatsby/memo/src/components/seo.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react'
+import { Helmet } from 'react-helmet'
+import { useStaticQuery, graphql } from 'gatsby'
+
+type Props = {
+  title: string
+  description: string
+  image: string
+  path: string
+}
+export const Seo: React.FC<Partial<Props>> = (props) => {
+  const data = useStaticQuery(graphql`
+    query GetSiteMetadata {
+      site {
+        siteMetadata {
+          description
+          image
+          siteUrl
+          title
+        }
+      }
+    }
+  `)
+
+  const defaults = data?.site?.siteMetadata
+  const title = props.title || defaults.title
+  const description = props.description || defaults.title
+  const image = new URL(props.image || defaults.image, defaults.siteUrl)
+  const url = new URL(props.path || '/', defaults.siteUrl)
+
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <link rel="canonical" href={url.href} />
+      {image && <meta name="image" content={image.href} />}
+
+      <meta property="og:url" content={url.href} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      {image && <meta property="og:image" content={image.href} />}
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      {image && <meta property="twitter:image" content={image.href} />}
+    </Helmet>
+  )
+}
--- gatsby/memo/src/pages/about.tsx
+++ gatsby/memo/src/pages/about.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import { Seo } from '../components/seo'
 
 export default function AboutPage() {
   return (
-    <main>
-      <h1>About This Site</h1>
-      <Link to="/">Back to home</Link>
-    </main>
+    <>
+      <Seo title="About this site" description="About this site" />
+      <main>
+        <h1>About This Site</h1>
+        <Link to="/">Back to home</Link>
+      </main>
+    </>
   )
 }
