7324f623d50b0b9bf3ab045a4783f8f4e19412a4
bobstrange <bob.1983.g@gmail.com>
2021-10-22 23:14:26 +0900

    feat: query image from GraphQL

--- gatsby/memo/src/pages/about.tsx
+++ gatsby/memo/src/pages/about.tsx
@@ -1,14 +1,35 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import { Layout } from '../components/layout'
+import { GatsbyImage, getImage, IGatsbyImageData } from 'gatsby-plugin-image'
 
-export default function AboutPage() {
+export const query = graphql`
+  query AboutImageQuery {
+    file(name: { eq: "cyclist" }) {
+      childImageSharp {
+        gatsbyImageData(placeholder: DOMINANT_COLOR)
+      }
+    }
+  }
+`
+type Props = {
+  data: {
+    file: IGatsbyImageData
+  }
+}
+export const AboutPage: React.FC<Props> = ({ data }) => {
   return (
     <>
       <Layout title="About this site" description="About this site">
+        <GatsbyImage
+          image={getImage(data.file) as IGatsbyImageData}
+          alt="image"
+        />
         <h1>About This Site</h1>
         <Link to="/">Back to home</Link>
       </Layout>
     </>
   )
 }
+
+export default AboutPage
