ffc1117972ebf111794d3487e5ecb641f6c6d1fb
bobstrange <bob.1983.g@gmail.com>
2021-11-25 21:48:33 +0900

    feat: install prettier

--- react/rts/src/index.tsx
+++ react/rts/src/index.tsx
@@ -1,11 +1,11 @@
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom'
 
 const App = () => {
   return (
     <div>
       <h1>Example app</h1>
     </div>
-  );
-};
+  )
+}
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'))
