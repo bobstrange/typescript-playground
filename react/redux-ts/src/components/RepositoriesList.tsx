05c64e96806b69b0c559404e782da4e37a88603b
bobstrange <bob.1983.g@gmail.com>
2021-11-29 00:01:02 +0900

    feat: show searched packages

--- react/redux-ts/src/components/RepositoriesList.tsx
+++ react/redux-ts/src/components/RepositoriesList.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import { useActions } from '../hooks/useActions'
+import { useTypedSelector } from '../hooks/useTypedSelector'
 
 export const RepositoriesList: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState('')
   const { searchRepositories } = useActions()
-
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  )
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     searchRepositories(searchTerm)
@@ -21,6 +24,15 @@ export const RepositoriesList: React.FC = () => {
         />
         <button type="submit">Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && (
+        <ul>
+          {data.map((name) => (
+            <li key={name}>{name}</li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
