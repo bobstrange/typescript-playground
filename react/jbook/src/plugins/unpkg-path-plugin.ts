56c9b56b32568aebf497a3799d8c5a0dc2ae14e0
bobstrange <bob.1983.g@gmail.com>
2021-12-04 23:18:41 +0900

    refactor: refactor onResolve function

--- react/jbook/src/plugins/unpkg-path-plugin.ts
+++ react/jbook/src/plugins/unpkg-path-plugin.ts
@@ -10,20 +10,22 @@ export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args)
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' }
+      // Handle root entry file of 'index.js'
+      build.onResolve({ filter: /^index\.js$/ }, () => {
+        return { path: 'index.js', namespace: 'a' }
+      })
+
+      // Handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: esbuild.OnResolveArgs) => {
+        return {
+          path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/')
+            .href,
+          namespace: 'a',
         }
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return {
-            path: new URL(
-              args.path,
-              'https://unpkg.com' + args.resolveDir + '/'
-            ).href,
-            namespace: 'a',
-          }
-        }
+      })
+
+      // Handle main file of a module
+      build.onResolve({ filter: /.*/ }, async (args: esbuild.OnResolveArgs) => {
         return {
           path: `https://unpkg.com/${args.path}`,
           namespace: 'a',
