f70078539da1807fffa59691302bfc3bdd9bdb8c
bobstrange <bob.1983.g@gmail.com>
2021-12-04 23:24:11 +0900

    refactor: make fetch-plugin

--- react/jbook/src/index.tsx
+++ react/jbook/src/index.tsx
@@ -2,6 +2,7 @@ import * as esbuild from 'esbuild-wasm'
 import { useEffect, useRef, useState } from 'react'
 import ReactDOM from 'react-dom'
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
+import { fetchPlugin } from './plugins/fetch-plugin'
 
 const App = () => {
   const ref = useRef<any>()
@@ -25,7 +26,7 @@ const App = () => {
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin(input)],
+      plugins: [unpkgPathPlugin(), fetchPlugin({ inputCode: input })],
       define: {
         'process.env.NODE_ENV': '"production"',
         global: 'window',
