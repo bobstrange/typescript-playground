f4f5f034a9c6f6f500a106264fcdd2a0b93c7c15
bobstrange <bob.1983.g@gmail.com>
2021-12-12 22:49:47 +0900

    feat: use query

--- nextjs/ts-next/app/src/framework/shopify/product/getAllProducts.ts
+++ nextjs/ts-next/app/src/framework/shopify/product/getAllProducts.ts
@@ -1,16 +1,24 @@
-const fetchData = async () => {
-  const url = 'https://jsonplaceholder.typicode.com/todos'
+import { getAllProductsQuery } from '../utils/queries/getAllProductsQuery'
+
+type FetchParams = {
+  query: string
+}
+
+const fetchData = async ({ query }: FetchParams) => {
+  const url = 'https://localhost:4000/graphql'
+
   const res = await fetch(url, {
-    method: 'GET',
+    method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
+    body: JSON.stringify({ query }),
   })
   const data = await res.json()
   return { data }
 }
 
 export const getAllProducts = async (): Promise<any[]> => {
-  const products = await fetchData()
+  const products = await fetchData({ query: getAllProductsQuery })
   return products.data
 }
