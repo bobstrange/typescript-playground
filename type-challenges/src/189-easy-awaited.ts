2f0e49202e1805ac9528f1e5bd60396042f2f5c6
bobstrange <bob.1983.g@gmail.com>
2021-12-16 00:03:41 +0900

    feat: example

--- type-challenges/src/189-easy-awaited.ts
+++ type-challenges/src/189-easy-awaited.ts
@@ -1,6 +1,14 @@
 type Hoge = Promise<number>
 
-type Unwrap<T> = T extends Promise<infer U> ? U : never
+type Unwrap<T> = T extends Promise<infer U> ? U : T
 
 type Fuga = Unwrap<Hoge>
 type Piyo = Unwrap<number>
+
+// 回答例 だと、Recursive に  MyAwaited を適用しているけど、題意を満たせないのでは？
+type MyAwaited<T> = T extends Promise<infer F>
+  ? F extends Promise<any>
+    ? MyAwaited<F>
+    : F
+  : T
+type Res = MyAwaited<Promise<Promise<number>>>
