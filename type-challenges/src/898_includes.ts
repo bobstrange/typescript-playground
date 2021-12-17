6c4d83f33e29ba92443bafb4b825dd003638bc8e
bobstrange <bob.1983.g@gmail.com>
2021-12-17 23:44:54 +0900

    fix: includes solution

--- type-challenges/src/898_includes.ts
+++ type-challenges/src/898_includes.ts
@@ -1,11 +1,17 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
 // これだと、リテラル型になってしまうので、string 型として判定させたい...
 // type Includes<T extends any[], U> = U extends keyof T ? true : false
 
 type Includes<T extends readonly any[], U> = U extends T[number] ? true : false
 
-type isPillarMen = Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'> // expected to be `false`
-type C1 = Includes<[1, 2, 3], 3>
-type C2 = Includes<[1, 2, 3], 4>
-type C3 = Includes<[boolean, 1, 2, 3], false>
-type C4 = Includes<[true, 1, 2, 3], boolean>
-type C5 = Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Wamuu'>
+type cases_898 = [
+  Expect<
+    Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'>, false>
+  >,
+  Expect<
+    Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Wamuu'>, true>
+  >,
+  Expect<Equal<Includes<[1, 2, 3], 2>, true>>,
+  Expect<Equal<Includes<[1, 2, 3], 4>, false>>
+]
