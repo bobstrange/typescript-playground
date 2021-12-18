3311e711607aacabc2675fe3e610184f72d48c3e
bobstrange <bob.1983.g@gmail.com>
2021-12-18 23:07:12 +0900

    wip: readonly

---
+++ type-challenges/src/8_readonly.ts
@@ -0,0 +1,36 @@
+import { Expect, Equal } from '@type-challenges/utils'
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+// わからなかった :-(
+// 回答例を見るに、一回指定された key に readonly をつけた上で、
+// 交差で、Generics の第二引数にあるもの以外のキーについてを定義し直す感じ
+// うーん、回答例を入れてみたけど、readonly がつかない :-()
+type MyReadonly2<T, K extends keyof T> = {
+  readonly [U in K]: T[U]
+} & {
+  [S in Exclude<keyof T, K>]: T[S]
+}
+
+const todo: MyReadonly2<Todo, 'title' | 'description'> = {
+  title: 'Hey',
+  description: 'foobar',
+  completed: false,
+}
+
+type cases_8 = [
+  Expect<
+    Equal<
+      MyReadonly2<Todo, 'title' | 'description'>,
+      {
+        readonly title: 'Hey'
+        readonly description: 'foobar'
+        completed: boolean
+      }
+    >
+  >
+]
