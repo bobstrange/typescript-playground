415c49b9d0499558e13219ddd27d756d472f1be3
bobstrange <bob.1983.g@gmail.com>
2021-12-19 11:08:14 +0900

    fix: fix readonly solution

--- type-challenges/src/8_readonly.ts
+++ type-challenges/src/8_readonly.ts
@@ -11,8 +11,9 @@ interface Todo {
 // 交差で、Generics の第二引数にあるもの以外のキーについてを定義し直す感じ
 // うーん、回答例を入れてみたけど、readonly がつかない :-(
 // どうやら expectation の書き方がまずかっただけみたい
-
-type MyReadonly2<T, K extends keyof T> = {
+// When K is not provided, it should make all properties readonly just like the normal Readonly<T>.
+// ↑の条件を忘れてたので、追加
+type MyReadonly2<T, K extends keyof T = keyof T> = {
   readonly [U in K]: T[U]
 } & {
   [S in Exclude<keyof T, K>]: T[S]
