592604aabc59ab733ef815b2485278e734cd73ad
bobstrange <bob.1983.g@gmail.com>
2021-12-19 11:28:04 +0900

    fix: fix readonly solution

--- type-challenges/src/9_deep_readonly.ts
+++ type-challenges/src/9_deep_readonly.ts
@@ -6,18 +6,28 @@ type X = {
     b: 'hi'
   }
   y: 'hey'
+  aaa: (a: string) => void
 }
 
-type DeepReadonly<T> = {
-  readonly [P in keyof T]: DeepReadonly<T[P]>
-}
+// Primitive などの場合と object の場合を分ける必要があった、、、
+type Primitive = number | string | boolean | bigint | symbol | undefined | null
+// eslint-disable-next-line @typescript-eslint/ban-types
+type Builtin = Primitive | Function | Date | Error | RegExp
 
-type Expected = {
+type DeepReadonly<T> = T extends Builtin
+  ? T
+  : T extends object
+  ? { readonly [U in keyof T]: DeepReadonly<T[U]> }
+  : never
+
+type Expected_9 = {
   readonly x: {
     readonly a: 1
     readonly b: 'hi'
   }
   readonly y: 'hey'
+  readonly aaa: (a: string) => void
 }
+type Actual_9 = DeepReadonly<X>
 
-type cases_9 = [Expect<Equal<DeepReadonly<X>, Expected>>]
+type cases_9 = [Expect<Equal<DeepReadonly<X>, Expected_9>>]
