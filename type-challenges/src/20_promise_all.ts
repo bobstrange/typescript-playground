f5ffb4646783dc7a9fe2c15a25d987c73e535457
bobstrange <bob.1983.g@gmail.com>
2021-12-22 23:15:17 +0900

    fix

--- type-challenges/src/20_promise_all.ts
+++ type-challenges/src/20_promise_all.ts
@@ -4,5 +4,9 @@ const promise3 = new Promise<string>((resolve, reject) => {
   setTimeout(resolve, 100, 'foo')
 })
 
-// expected to be `Promise<[number, number, string]>`
+declare function PromiseAll<T extends any[]>(
+  values: readonly [...T]
+): Promise<{ [K in keyof T]: T[K] extends Promise<infer R> ? R : T[K] }>
+
 const p = Promise.all([promise1, promise2, promise3] as const)
+const p2 = PromiseAll([promise1, promise2, promise3] as const)
