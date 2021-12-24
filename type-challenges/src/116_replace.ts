eeb2c499962ec210775bc1ec40dd77d30efce662
bobstrange <bob.1983.g@gmail.com>
2021-12-24 18:38:05 +0900

    feat: add testcase

--- type-challenges/src/116_replace.ts
+++ type-challenges/src/116_replace.ts
@@ -6,6 +6,21 @@ type Replace<
   To extends string
 > = S extends `${infer First}${From}${infer Rest}` ? `${First}${To}${Rest}` : S
 
-type case_116 = Expect<
-  Equal<Replace<'types are fun!', 'fun', 'awesome'>, 'types are awesome!'>
->
+// First とか Rest とかが '' になるパターンこれで対応できてるか？
+type case_116 = [
+  Expect<
+    Equal<Replace<'types are fun!', 'fun', 'awesome'>, 'types are awesome!'>
+  >,
+  Expect<
+    Equal<
+      Replace<'types are fun!', 'types are', 'TypeScript is'>,
+      'TypeScript is fun!'
+    >
+  >,
+  Expect<
+    Equal<
+      Replace<'types are fun!', 'fun!', 'interesting?'>,
+      'types are interesting?'
+    >
+  >
+]
