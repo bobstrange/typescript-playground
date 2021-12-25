18fd4bb58c0632cfaaacfc993383cc0fe2b207c2
bobstrange <bob.1983.g@gmail.com>
2021-12-25 23:25:50 +0900

    feat: append argument solution

---
+++ type-challenges/src/191_append_argument.ts
@@ -0,0 +1,26 @@
+import { Expect, Equal } from '@type-challenges/utils'
+
+type Fn = (a: number, b: string) => number
+
+// type AppendArgument<F extends Function, T> = F extends (
+//   ...args: infer U
+// ) => infer R
+//   ? // ...args を先に持っていきたいが、Rest parameter は最後にしないとダメ、、、
+//     (c: T, ...args: U) => R
+//   : never
+
+// (...args: [...U, T]) 型の方でも、配列の展開が可能
+type AppendArgument<Fn, T> = Fn extends (...args: infer U) => infer R
+  ? (...args: [...U, T]) => R
+  : never
+type hoge = AppendArgument<Fn, boolean>
+
+type cases_191 = [
+  Expect<
+    Equal<
+      AppendArgument<Fn, boolean>,
+      (a: number, b: string, c: boolean) => number
+    >
+  >
+]
+// expected be (a: number, b: string, x: boolean) => number
