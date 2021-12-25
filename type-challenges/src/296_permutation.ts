c7c54b5b775d030c9e3f783e1d7921a5f9ad4037
bobstrange <bob.1983.g@gmail.com>
2021-12-25 23:37:23 +0900

    give up permutation

---
+++ type-challenges/src/296_permutation.ts
@@ -0,0 +1,19 @@
+import { Expect, Equal } from '@type-challenges/utils'
+
+// Union をどうやって受ければ良いのかがわからない、、、
+// しかたが無いので回答例を見る
+// https://github.com/type-challenges/type-challenges/issues/614
+
+type cases_296 = [
+  Expect<
+    Equal<
+      Permutation<'A' | 'B' | 'C'>,
+      | ['A', 'B', 'C']
+      | ['A', 'C', 'B']
+      | ['B', 'A', 'C']
+      | ['B', 'C', 'A']
+      | ['C', 'A', 'B']
+      | ['C', 'B', 'A']
+    >
+  >
+]
