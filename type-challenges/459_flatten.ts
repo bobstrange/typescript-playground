e00fbbf3437fc7b83f58c597148eb6e51945d948
bobstrange <bob.1983.g@gmail.com>
2021-12-26 23:58:30 +0900

    feat: flatten

---
+++ type-challenges/459_flatten.ts
@@ -0,0 +1,17 @@
+import { Expect, Equal } from '@type-challenges/utils'
+
+type Flatten<T> = T extends any[]
+  ? T extends []
+    ? []
+    : T extends [infer F]
+    ? [...Flatten<F>]
+    : T extends [infer F, ...infer R]
+    ? [...Flatten<F>, ...Flatten<R>]
+    : never
+  : [T]
+
+type hoge = Flatten<[1, 2, [3, 4], [[[5]]]]>
+
+type cases_459 = [
+  Expect<Equal<Flatten<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, 5]>>
+]
