86153e47e405b6c58e0e3220a6cf9859bba0fd82
bobstrange <bob.1983.g@gmail.com>
2021-12-27 23:12:08 +0900

    memo

--- type-challenges/src/531_string_to_union.ts
+++ type-challenges/src/531_string_to_union.ts
@@ -1,5 +1,7 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+// String を再帰的に、型引数 U のタプルに入れていく
+// 型引数 U が空配列の場合は、 '' にして、そうでない場合は、U[number] でタプルから union にする
 type StringToUnion<
   S extends string,
   U extends string[] = []
