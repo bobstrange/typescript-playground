3f926698b4bf41b4904ed69e0f34ac87d0787631
bobstrange <bob.1983.g@gmail.com>
2021-12-27 23:30:47 +0900

    memo

--- type-challenges/src/599_merge.ts
+++ type-challenges/src/599_merge.ts
@@ -1,6 +1,9 @@
 import { Expect, Equal } from '@type-challenges/utils'
 
 type Merge<T, U> = {
+  // U を優先して { [K in keyof U]: U[K] } と考える
+  // その上で T を付加する { [K in keyof T | keyof U]: ... }
+  // K が keyof U である場合は U[K], K が keyof T である場合は T[K] それ以外は never となる
   [K in keyof T | keyof U]: K extends keyof U
     ? U[K]
     : K extends keyof T
