834143d836e9510d7093bb5d44330e95c4356d79
bobstrange <bob.1983.g@gmail.com>
2021-12-27 22:58:33 +0900

    memo

--- type-challenges/src/529_absolute.ts
+++ type-challenges/src/529_absolute.ts
@@ -1,5 +1,6 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+// T が string の場合もあるので、T extends '-{infer V}` ではなく、`${T} extends `-${infer V}` とする
 type Absolute<T extends number | string | bigint> = `${T}` extends `-${infer V}`
   ? V
   : `${T}`
