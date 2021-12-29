a4caef8f4861038a9255d6ddd7974b17ae746eb9
bobstrange <bob.1983.g@gmail.com>
2021-12-29 22:59:50 +0900

    wip: diff solution

---
+++ type-challenges/src/645_diff.ts
@@ -0,0 +1,43 @@
+import { Expect, Equal } from '@type-challenges/utils'
+
+type O_645_1 = {
+  a: 'test'
+  b: [1, 2, 3]
+  c: {
+    d: 'test'
+  }
+}
+
+type O_645_2 = {
+  a: 'test'
+  b: [1, 2, 3, 4]
+  e: 'test2'
+}
+
+type Expected_645 = {
+  b: [1, 2, 3, 4]
+  c: {
+    d: 'test'
+  }
+  e: 'test2'
+}
+
+// これだと、b の差分が出てくれない
+// [K in keyof T | keyof U] にして、never の値の key を除外できる？
+type Diff<T, U> = {
+  [K in
+    | Exclude<keyof T, keyof U>
+    | Exclude<keyof U, keyof T>]: K extends keyof T & keyof U
+    ? T[K] extends U[K]
+      ? never
+      : U[K]
+    : K extends keyof T
+    ? T[K]
+    : K extends keyof U
+    ? U[K]
+    : never
+}
+
+type hoge = Diff<O_645_1, O_645_2>
+
+type case_645 = Expect<Equal<Diff<O_645_1, O_645_2>, Expected_645>>
