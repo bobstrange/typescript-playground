cb325c3d1fb3979e782f247b8af6957798dedcf1
bobstrange <bob.1983.g@gmail.com>
2021-12-31 00:11:27 +0900

    feat: configure editor

--- react/jbook/src/components/code-editor.tsx
+++ react/jbook/src/components/code-editor.tsx
@@ -1,5 +1,21 @@
 import MonacoEditor from '@monaco-editor/react'
 
 export const CodeEditor = () => {
-  return <MonacoEditor height={500} language="javascript" theme="vs-dark" />
+  return (
+    <MonacoEditor
+      height={500}
+      language="javascript"
+      theme="vs-dark"
+      options={{
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+    />
+  )
 }
