2273db6e0a48f1397bbdd4c90b718dfc85465dd2
bobstrange <bob.1983.g@gmail.com>
2021-12-30 23:45:27 +0900

    fix: fix up warnings

--- react/jbook/src/index.tsx
+++ react/jbook/src/index.tsx
@@ -9,7 +9,6 @@ const App = () => {
   const ref = useRef<any>()
   const iframe = useRef<any>()
   const [input, setInput] = useState('')
-  const [code, setCode] = useState('')
 
   const startService = async () => {
     const service = await esbuild.startService({
@@ -37,7 +36,6 @@ const App = () => {
       },
     })
 
-    // setCode(result.outputFiles[0].text)
     iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
   }
 
@@ -74,8 +72,12 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
-      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+      <iframe
+        title="code-preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
     </div>
   )
 }
