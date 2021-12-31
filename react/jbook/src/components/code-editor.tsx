c7c780dd5e7abd6e0649c7403162b804faa43dd9
bobstrange <bob.1983.g@gmail.com>
2021-12-31 13:23:53 +0900

    feat: add formatter

--- react/jbook/src/components/code-editor.tsx
+++ react/jbook/src/components/code-editor.tsx
@@ -1,5 +1,9 @@
 import MonacoEditor, { OnMount } from '@monaco-editor/react'
 
+import prettier from 'prettier'
+import parser from 'prettier/parser-babel'
+import { useRef } from 'react'
+
 type CodeEditorProps = {
   initialValue: string
   onChange: (value: string) => void
@@ -9,7 +13,9 @@ export const CodeEditor: React.FC<CodeEditorProps> = ({
   initialValue,
   onChange,
 }) => {
+  const editorRef = useRef<any>()
   const onMount: OnMount = (editor, monaco) => {
+    editorRef.current = editor
     editor.onDidChangeModelContent(() => {
       onChange(editor.getValue())
     })
@@ -17,23 +23,38 @@ export const CodeEditor: React.FC<CodeEditorProps> = ({
     editor.getModel()?.updateOptions({ tabSize: 2 })
   }
 
+  const onFormatClick = () => {
+    const unformatted = editorRef.current.getModel().getValue()
+    const formatted = prettier.format(unformatted, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: false,
+      singleQuote: true,
+    })
+    editorRef.current.setValue(formatted)
+  }
+
   return (
-    <MonacoEditor
-      onMount={onMount}
-      value={initialValue}
-      height={500}
-      language="javascript"
-      theme="vs-dark"
-      options={{
-        wordWrap: 'on',
-        minimap: { enabled: false },
-        showUnused: false,
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-      }}
-    />
+    <div>
+      <button onClick={onFormatClick}>Format</button>
+      <MonacoEditor
+        onMount={onMount}
+        value={initialValue}
+        height={500}
+        language="javascript"
+        theme="vs-dark"
+        options={{
+          wordWrap: 'on',
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+      />
+    </div>
   )
 }
