f36c14211ca766911ae721a2e62d4a42926cff17
bobstrange <bob.1983.g@gmail.com>
2022-01-01 20:58:35 +0900

    feat: drop char solution

---
+++ type-challenges/src/2070_dropchar.ts
@@ -0,0 +1,13 @@
+import { Expect, Equal } from '@type-challenges/utils'
+
+type DropChar<
+  S extends string,
+  R extends string
+> = S extends `${infer First}${R}${infer Rest}`
+  ? DropChar<`${First}${Rest}`, R>
+  : S
+
+type hoge = DropChar<' b u t t e r f l y ! ', ' '>
+type case_2070 = Expect<
+  Equal<DropChar<' b u t t e r f l y ! ', ' '>, 'butterfly!'>
+>
