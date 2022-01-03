c2ddef1ed0d41562b04f629e0f13431e89c1dbe2
bobstrange <bob.1983.g@gmail.com>
2022-01-03 20:28:03 +0900

    feat: object entries solution 2

--- type-challenges/src/2946_object_entries.ts
+++ type-challenges/src/2946_object_entries.ts
@@ -1,8 +1,12 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
-type ObjectEntries<T> = {
-  [K in keyof T]: [K, T[K]]
-}[keyof T]
+// type ObjectEntries<T> = {
+//   [K in keyof T]: [K, T[K]]
+// }[keyof T]
+
+// index signature を使わなくても、K を Generics の第二引数に指定して
+// K extends keyof T で、K の型を解決できる
+type ObjectEntries<T, K = keyof T> = K extends keyof T ? [K, T[K]] : never
 
 type case_2946 = Expect<
   Equal<
