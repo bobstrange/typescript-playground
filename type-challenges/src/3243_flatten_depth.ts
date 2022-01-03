81eeb3576ec0cd7019d24d86e42f11abcd3bed2e
bobstrange <bob.1983.g@gmail.com>
2022-01-03 21:11:03 +0900

    wip

---
+++ type-challenges/src/3243_flatten_depth.ts
@@ -0,0 +1,6 @@
+import { Expect, Equal } from '@type-challenges/utils'
+
+type cases_3243 = [
+  Expect<Equal<FlattenDepth<[1, 2, [3, 4], [[[5]]]], 2>, [1, 2, 3, 4, [5]]>>,
+  Expect<Equal<FlattenDepth<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, [[5]]]>>
+]
