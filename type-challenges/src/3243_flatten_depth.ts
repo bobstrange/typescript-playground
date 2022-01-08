fa8c9cced486027b0764d3c5aa055e3f14384333
bobstrange <bob.1983.g@gmail.com>
2022-01-08 23:36:08 +0900

    feat: flatten depth solution

--- type-challenges/src/3243_flatten_depth.ts
+++ type-challenges/src/3243_flatten_depth.ts
@@ -1,5 +1,21 @@
 import { Expect, Equal } from '@type-challenges/utils'
 
+type Flatten<T extends unknown[]> = T extends [infer First, ...infer Rest]
+  ? First extends unknown[]
+    ? [...First, ...Flatten<Rest>]
+    : [First, ...Flatten<Rest>]
+  : []
+
+type FlattenDepth<
+  T extends unknown[],
+  Depth extends number = 1,
+  L extends number[] = []
+> = Flatten<T> extends T
+  ? T
+  : L['length'] extends Depth
+  ? T
+  : FlattenDepth<Flatten<T>, Depth, [...L, 1]>
+
 type cases_3243 = [
   Expect<Equal<FlattenDepth<[1, 2, [3, 4], [[[5]]]], 2>, [1, 2, 3, 4, [5]]>>,
   Expect<Equal<FlattenDepth<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, [[5]]]>>
