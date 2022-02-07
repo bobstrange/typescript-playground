732616fc625ddae201753b37342d539c8a1eb412
bobstrange <bob.1983.g@gmail.com>
2022-02-07 22:44:47 +0900

    feat: add help

--- type-challenges/scripts/get_template.sh
+++ type-challenges/scripts/get_template.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+usage() {
+    cat <<-EOF
+
+    Usage: $0 <template_name>
+
+    example:
+      $0 3312-easy-parameters
+
+EOF
+    exit 1
+}
+
+while getopts ":h" opt; do
+  case $opt in
+    h)
+        usage
+        ;;
+    *)
+    ;;
+  esac
+done
+
 BASE_URL=https://raw.githubusercontent.com/type-challenges/type-challenges/master/questions
 BASE_DIR=${2:-'src/second'}
 
