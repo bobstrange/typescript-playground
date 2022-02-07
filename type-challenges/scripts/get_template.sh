03a3d3c61ad71f02cac99ac312a101d047e1e73e
bobstrange <bob.1983.g@gmail.com>
2022-02-07 22:36:30 +0900

    feat: change script

--- type-challenges/scripts/get_template.sh
+++ type-challenges/scripts/get_template.sh
@@ -3,4 +3,11 @@
 BASE_URL=https://raw.githubusercontent.com/type-challenges/type-challenges/master/questions
 BASE_DIR=${2:-'src/second'}
 
-curl "${BASE_URL}/${1}/template.ts" > ${BASE_DIR}/${1}.ts
+FILE_PATH=${BASE_DIR}/${1}.ts
+if [[ -f ${FILE_PATH} ]]; then
+  echo "File ${FILE_PATH} already exists"
+  exit 1
+fi
+
+curl "${BASE_URL}/${1}/template.ts" > ${FILE_PATH}
+curl "${BASE_URL}/${1}/test-cases.ts" >> ${FILE_PATH}
