8c64508b2d8105c7f0f77e26b5103b322571ee2a
bobstrange <bob.1983.g@gmail.com>
2022-02-07 22:25:02 +0900

    feat: push solution

---
+++ type-challenges/src/second/3057_push.ts
@@ -0,0 +1,9 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type Push<T extends unknown[], U> = [...T, U]
+
+type cases_3057 = [
+  Expect<Equal<Push<[], 1>, [1]>>,
+  Expect<Equal<Push<[1, 2], '3'>, [1, 2, '3']>>,
+  Expect<Equal<Push<['1', 2, '3'], boolean>, ['1', 2, '3', boolean]>>
+]
