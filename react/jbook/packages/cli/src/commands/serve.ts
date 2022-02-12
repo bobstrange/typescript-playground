664349582575c23ab793099694e7263c9fb94837
bobstrange <bob.1983.g@gmail.com>
2022-02-12 23:24:55 +0900

    feat: scaffolding serve

--- react/jbook/packages/cli/src/commands/serve.ts
+++ react/jbook/packages/cli/src/commands/serve.ts
@@ -1,9 +1,13 @@
 import { Command } from 'commander'
+import { serve } from 'local-api'
 
 export const serveCommand = new Command()
   .command('serve [filename]')
   .description('opean a file for editing')
   .option('-p, --port <nunmber>', 'port number', '8080')
-  .action((filename = 'notebook.js', options) => {
+  .action((filename = 'notebook.js', options: { port: string }) => {
     console.log(filename, options)
+
+    const port = parseInt(options.port)
+    serve({ port, filename, dir: '/' })
   })
--- react/jbook/packages/local-api/src/index.ts
+++ react/jbook/packages/local-api/src/index.ts
@@ -1,3 +1,13 @@
-export default () => {
-  console.log('server is running')
+export const serve = ({
+  port,
+  filename,
+  dir,
+}: {
+  port: number
+  filename: string
+  dir: string
+}) => {
+  console.log('port: ', port)
+  console.log('filename: ', filename)
+  console.log('dir: ', dir)
 }
