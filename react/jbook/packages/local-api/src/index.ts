a177920ffca6c63af011ba2b4623e0f3a06dd4ea
bobstrange <bob.1983.g@gmail.com>
2022-02-14 22:55:03 +0900

    feat: proxy to local react server

--- react/jbook/packages/local-api/src/index.ts
+++ react/jbook/packages/local-api/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { createProxyMiddleware } from 'http-proxy-middleware'
 
 export const serve = ({
   port,
@@ -11,6 +12,13 @@ export const serve = ({
 }) => {
   const app = express()
 
+  app.use(
+    createProxyMiddleware({
+      target: 'http://localhost:3000',
+      ws: true,
+      logLevel: 'silent',
+    })
+  )
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject)
   })
