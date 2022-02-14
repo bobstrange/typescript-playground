97cef5eccc8ce8ff819573eecf51bff782dcf36e
bobstrange <bob.1983.g@gmail.com>
2022-02-14 23:20:39 +0900

    fix package path

--- react/jbook/packages/local-api/src/index.ts
+++ react/jbook/packages/local-api/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { createProxyMiddleware } from 'http-proxy-middleware'
+import path from 'path'
 
 export const serve = ({
   port,
@@ -12,13 +13,15 @@ export const serve = ({
 }) => {
   const app = express()
 
-  app.use(
-    createProxyMiddleware({
-      target: 'http://localhost:3000',
-      ws: true,
-      logLevel: 'silent',
-    })
-  )
+  const packagePath = require.resolve('local-client/build/index.html')
+  app.use(express.static(path.dirname(packagePath)))
+  // app.use(
+  //   createProxyMiddleware({
+  //     target: 'http://localhost:3000',
+  //     ws: true,
+  //     logLevel: 'silent',
+  //   })
+  // )
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject)
   })
