5aacf6558abac51b4774f4f305ac20f98f86152e
bobstrange <bob.1983.g@gmail.com>
2022-02-24 22:57:11 +0900

    feat: replace memo

--- type-challenges/src/second/0116-medium-replace.ts
+++ type-challenges/src/second/0116-medium-replace.ts
@@ -9,7 +9,10 @@ type Replace<
   : S extends `${infer L}${From}${infer R}`
   ? `${L}${To}${R}`
   : S
-
+/**
+ * まずは、 S extends `${infer L}${From}${infer R}` の場合に From を To に置き換えるようにする
+ * このままだと From が '' の場合に、想定通りに動かないので、From が '' の場合は何も変換しないようにする
+ */
 type hoge = Replace<'foobarbar', '', 'foo'>
 type cases = [
   Expect<Equal<Replace<'foobar', 'bar', 'foo'>, 'foofoo'>>,
