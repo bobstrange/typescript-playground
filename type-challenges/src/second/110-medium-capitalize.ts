ad18c0157955bfab6d705a3c40c14c5fbf5065c0
bobstrange <bob.1983.g@gmail.com>
2022-02-24 22:45:16 +0900

    feat: do not use Uppercase

--- type-challenges/src/second/110-medium-capitalize.ts
+++ type-challenges/src/second/110-medium-capitalize.ts
@@ -1,7 +1,38 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+interface CapitalizedChars {
+  a: 'A'
+  b: 'B'
+  c: 'C'
+  d: 'D'
+  e: 'E'
+  f: 'F'
+  g: 'G'
+  h: 'H'
+  i: 'I'
+  j: 'J'
+  k: 'K'
+  l: 'L'
+  m: 'M'
+  n: 'N'
+  o: 'O'
+  p: 'P'
+  q: 'Q'
+  r: 'R'
+  s: 'S'
+  t: 'T'
+  u: 'U'
+  v: 'V'
+  w: 'W'
+  x: 'X'
+  y: 'Y'
+  z: 'Z'
+}
+
 type MyCapitalize<S extends string> = S extends `${infer First}${infer Rest}`
-  ? `${Uppercase<First>}${Rest}`
+  ? `${First extends keyof CapitalizedChars
+      ? CapitalizedChars[First]
+      : First}${Rest}`
   : S
 
 type cases = [
