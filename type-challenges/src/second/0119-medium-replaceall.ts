3649c024d711c9688d71f7441c0ea615100d9d04
bobstrange <bob.1983.g@gmail.com>
2022-02-24 23:06:15 +0900

    feat: replace all memo

--- type-challenges/src/second/0119-medium-replaceall.ts
+++ type-challenges/src/second/0119-medium-replaceall.ts
@@ -9,6 +9,13 @@ type ReplaceAll<
   ? `${L}${To}${ReplaceAll<R, From, To>}`
   : S
 
+/**
+ * 0116-medium-replace.ts と似ている
+ * 違う部分は、R に From が含まれている場合は Replace する必要があるということだけなので
+ * `${L}${To}${ReplaceAll<R, From, To>}` とする
+ * ※ 単に、再帰的に ReplaceAll の型引数に `${L}${To}${R}` とすると、置換後の文字列を元に置換が行われてしまう
+ */
+
 type cases = [
   Expect<Equal<ReplaceAll<'foobar', 'bar', 'foo'>, 'foofoo'>>,
   Expect<Equal<ReplaceAll<'foobar', 'bag', 'foo'>, 'foobar'>>,
