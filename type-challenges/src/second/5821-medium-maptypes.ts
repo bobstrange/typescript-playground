2a7280a13b780a00f1f56554b637387d2710891c
bobstrange <bob.1983.g@gmail.com>
2022-02-25 22:26:58 +0900

    wip 5821

---
+++ type-challenges/src/second/5821-medium-maptypes.ts
@@ -0,0 +1,72 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type MapTypes<T, R> = R extends { mapFrom: infer U; mapTo: infer V }
+  ? T extends { [key in infer TKey]: infer TValue }
+    ? TValue extends U
+      ? { [key in TKey]: V }
+      : T
+    : never
+  : never
+
+type cases = [
+  Expect<
+    Equal<
+      MapTypes<{ stringToArray: string }, { mapFrom: string; mapTo: [] }>,
+      { stringToArray: [] }
+    >
+  >,
+  Expect<
+    Equal<
+      MapTypes<{ stringToNumber: string }, { mapFrom: string; mapTo: number }>,
+      { stringToNumber: number }
+    >
+  >,
+  Expect<
+    Equal<
+      MapTypes<
+        { stringToNumber: string; skipParsingMe: boolean },
+        { mapFrom: string; mapTo: number }
+      >,
+      { stringToNumber: number; skipParsingMe: boolean }
+    >
+  >,
+  Expect<
+    Equal<
+      MapTypes<
+        { date: string },
+        { mapFrom: string; mapTo: Date } | { mapFrom: string; mapTo: null }
+      >,
+      { date: null | Date }
+    >
+  >,
+  Expect<
+    Equal<
+      MapTypes<{ date: string }, { mapFrom: string; mapTo: Date | null }>,
+      { date: null | Date }
+    >
+  >,
+  Expect<
+    Equal<
+      MapTypes<
+        { fields: Record<string, boolean> },
+        { mapFrom: Record<string, boolean>; mapTo: string[] }
+      >,
+      { fields: string[] }
+    >
+  >,
+  Expect<
+    Equal<
+      MapTypes<{ name: string }, { mapFrom: boolean; mapTo: never }>,
+      { name: string }
+    >
+  >,
+  Expect<
+    Equal<
+      MapTypes<
+        { name: string; date: Date },
+        { mapFrom: string; mapTo: boolean } | { mapFrom: Date; mapTo: string }
+      >,
+      { name: boolean; date: string }
+    >
+  >
+]
