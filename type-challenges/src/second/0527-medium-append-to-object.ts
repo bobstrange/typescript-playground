f0bc0ee848fcfad9749de61a46143b112a31be48
bobstrange <bob.1983.g@gmail.com>
2022-02-26 23:54:35 +0900

    feat: append to object solution

---
+++ type-challenges/src/second/0527-medium-append-to-object.ts
@@ -0,0 +1,66 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type AppendToObject<T, U extends string, V> = {
+  [K in keyof T | U]: K extends keyof T ? T[K] : V
+}
+
+/**
+ * intersection type を使用すると、テストケースをパスしない :-(
+ * AppendToObject<T, U extends string, V> = T & { [K in U]: V }
+ *
+ * なので、まずは、元の T をそのまま表す mapped type を用意する
+ *
+ * type AppendToObject<T, U extends string, V> = {
+ *   [K in keyof T]: T[K]
+ * }
+ *
+ * keyof T は union type であるためさらに、それに U も追加する
+ * そうすると、T[K] の部分で型エラーになるので、K extends keyof T のときは T[K] そうでないときは V とすれば良い
+ */
+type test1 = {
+  key: 'cat'
+  value: 'green'
+}
+
+type testExpect1 = {
+  key: 'cat'
+  value: 'green'
+  home: boolean
+}
+
+type test2 = {
+  key: 'dog' | undefined
+  value: 'white'
+  sun: true
+}
+
+type testExpect2 = {
+  key: 'dog' | undefined
+  value: 'white'
+  sun: true
+  home: 1
+}
+
+type test3 = {
+  key: 'cow'
+  value: 'yellow'
+  sun: false
+}
+
+type testExpect3 = {
+  key: 'cow'
+  value: 'yellow'
+  sun: false
+  isMotherRussia: false | undefined
+}
+
+type cases = [
+  Expect<Equal<AppendToObject<test1, 'home', boolean>, testExpect1>>,
+  Expect<Equal<AppendToObject<test2, 'home', 1>, testExpect2>>,
+  Expect<
+    Equal<
+      AppendToObject<test3, 'isMotherRussia', false | undefined>,
+      testExpect3
+    >
+  >
+]
