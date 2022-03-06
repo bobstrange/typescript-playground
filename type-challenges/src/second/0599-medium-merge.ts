1dda19c1d5d6e07b31cf4122ef2eb8ae1380d595
bobstrange <bob.1983.g@gmail.com>
2022-03-07 00:04:15 +0900

    feat: merge solution

---
+++ type-challenges/src/second/0599-medium-merge.ts
@@ -0,0 +1,31 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type Merge<F, S> = {
+  [K in keyof F | keyof S]: K extends keyof S
+    ? S[K]
+    : K extends keyof F
+    ? F[K]
+    : never
+}
+
+type Foo = {
+  a: number
+  b: string
+}
+type Bar = {
+  b: number
+  c: boolean
+}
+
+type cases = [
+  Expect<
+    Equal<
+      Merge<Foo, Bar>,
+      {
+        a: number
+        b: number
+        c: boolean
+      }
+    >
+  >
+]
