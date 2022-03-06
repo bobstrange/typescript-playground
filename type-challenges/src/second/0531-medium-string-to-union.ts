1f5cb048d3dd6fc0c21076b861969d293319b755
bobstrange <bob.1983.g@gmail.com>
2022-03-06 23:58:23 +0900

    memo

--- type-challenges/src/second/0531-medium-string-to-union.ts
+++ type-challenges/src/second/0531-medium-string-to-union.ts
@@ -4,6 +4,11 @@ type StringToUnion<T extends string> = T extends `${infer First}${infer Rest}`
   ? First | StringToUnion<Rest>
   : never
 
+/**
+ * String をループさせて、1 文字ずつ取得して、 union の左辺にする
+ * 残りについては、再帰的に StringToUnion を呼び出す
+ */
+
 type cases = [
   Expect<Equal<StringToUnion<''>, never>>,
   Expect<Equal<StringToUnion<'t'>, 't'>>,
