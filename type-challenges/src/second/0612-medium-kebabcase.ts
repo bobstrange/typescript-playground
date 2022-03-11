abba7b35326f37377aaac842701944593708486e
bobstrange <bob.1983.g@gmail.com>
2022-03-11 23:18:19 +0900

    feat: kebabcase solution

---
+++ type-challenges/src/second/0612-medium-kebabcase.ts
@@ -0,0 +1,19 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type KebabCase<S> = S extends `${infer L}${infer R}`
+  ? R extends Uncapitalize<R>
+    ? `${Uncapitalize<L>}${KebabCase<R>}`
+    : `${Uncapitalize<L>}-${KebabCase<Uncapitalize<R>>}`
+  : S
+
+type cases = [
+  Expect<Equal<KebabCase<'FooBarBaz'>, 'foo-bar-baz'>>,
+  Expect<Equal<KebabCase<'fooBarBaz'>, 'foo-bar-baz'>>,
+  Expect<Equal<KebabCase<'foo-bar'>, 'foo-bar'>>,
+  Expect<Equal<KebabCase<'foo_bar'>, 'foo_bar'>>,
+  Expect<Equal<KebabCase<'Foo-Bar'>, 'foo--bar'>>,
+  Expect<Equal<KebabCase<'ABC'>, 'a-b-c'>>,
+  Expect<Equal<KebabCase<'-'>, '-'>>,
+  Expect<Equal<KebabCase<''>, ''>>,
+  Expect<Equal<KebabCase<'😎'>, '😎'>>
+]
