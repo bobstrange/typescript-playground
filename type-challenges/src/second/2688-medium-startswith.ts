261481038b51d23d55b672d072823355106526f7
bobstrange <bob.1983.g@gmail.com>
2022-03-26 22:49:11 +0900

    feat: starts with solution

---
+++ type-challenges/src/second/2688-medium-startswith.ts
@@ -0,0 +1,11 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type StartsWith<T extends string, U extends string> = T extends `${U}${infer R}`
+  ? true
+  : false
+
+type cases = [
+  Expect<Equal<StartsWith<'abc', 'ac'>, false>>,
+  Expect<Equal<StartsWith<'abc', 'ab'>, true>>,
+  Expect<Equal<StartsWith<'abc', 'abcd'>, false>>
+]
