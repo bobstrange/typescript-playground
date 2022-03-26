14defeb2a0e482249acb616839943d506e0f706a
bobstrange <bob.1983.g@gmail.com>
2022-03-26 22:50:45 +0900

    feat: ends with solution

---
+++ type-challenges/src/second/2693-medium-endswith.ts
@@ -0,0 +1,10 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+type EndsWith<T extends string, U extends string> = T extends `${infer L}${U}`
+  ? true
+  : false
+
+type cases = [
+  Expect<Equal<EndsWith<'abc', 'bc'>, true>>,
+  Expect<Equal<EndsWith<'abc', 'abc'>, true>>,
+  Expect<Equal<EndsWith<'abc', 'd'>, false>>
+]
