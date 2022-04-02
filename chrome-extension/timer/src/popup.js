83772d3a80fbd4169f6c64e3daf986b387455150
bobstrange <bob.1983.g@gmail.com>
2022-04-02 23:54:59 +0900

    feat: alarm api

--- chrome-extension/timer/src/popup.js
+++ chrome-extension/timer/src/popup.js
@@ -1,20 +1,21 @@
 const timeElement = document.getElementById('time')
 const nameElement = document.getElementById('name')
-const currentTime = new Date().toLocaleTimeString()
-timeElement.textContent = `The time is ${currentTime}`
-
-chrome.action.setBadgeText(
-  {
-    text: 'TIME',
-  },
-  () => {
-    console.log('Finished setting badge text')
-  }
-)
+const timerElement = document.getElementById('timer')
 
 chrome.storage.sync.get(['name'], (res) => {
   const name = res.name ?? '???'
   nameElement.textContent = `Your name is ${name}`
 })
 
-console.log(this)
+const updateTimeElements = () => {
+  chrome.storage.local.get(['timer'], (res) => {
+    const time = res.timer ?? 0
+    timerElement.textContent = `You've been here for ${time} seconds`
+  })
+
+  const currentTime = new Date().toLocaleTimeString()
+  timeElement.textContent = `The time is ${currentTime}`
+}
+
+updateTimeElements()
+setInterval(updateTimeElements, 1000)
