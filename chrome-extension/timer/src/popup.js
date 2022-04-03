5e78f8a813162bbfb51b6c2ed1b82de695c156c2
bobstrange <bob.1983.g@gmail.com>
2022-04-03 23:26:22 +0900

    feat: start/stop/reset feature

--- chrome-extension/timer/src/popup.js
+++ chrome-extension/timer/src/popup.js
@@ -19,3 +19,26 @@ const updateTimeElements = () => {
 
 updateTimeElements()
 setInterval(updateTimeElements, 1000)
+
+const startButton = document.getElementById('start')
+const stopButton = document.getElementById('stop')
+const resetButton = document.getElementById('reset')
+
+startButton.addEventListener('click', () => {
+  chrome.storage.local.set({
+    isRunning: true,
+  })
+})
+
+stopButton.addEventListener('click', () => {
+  chrome.storage.local.set({
+    isRunning: false,
+  })
+})
+
+resetButton.addEventListener('click', () => {
+  chrome.storage.local.set({
+    timer: 0,
+    isRunning: false,
+  })
+})
