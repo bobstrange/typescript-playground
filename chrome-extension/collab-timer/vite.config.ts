8f670fcf041269c7e71e1e6e07056687b08e95d4
bobstrange <bob.1983.g@gmail.com>
2022-04-08 23:53:57 +0900

    fix: fix vite config

--- chrome-extension/collab-timer/vite.config.ts
+++ chrome-extension/collab-timer/vite.config.ts
@@ -1,7 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import copy from 'rollup-plugin-copy'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'src/main.tsx'),
+        background: resolve(__dirname, 'src/background/background.ts'),
+      },
+      output: {
+        entryFileNames: '[name].js',
+        dir: 'dist',
+      },
+    },
+  },
+  plugins: [
+    react(),
+    copy({
+      verbose: true,
+      hook: 'writeBundle',
+      targets: [
+        {
+          src: 'manifets.json',
+          dest: 'dist',
+        },
+        {
+          src: 'public/*',
+          dest: 'dist/public',
+        },
+        {
+          src: 'index.html',
+          dest: 'dist',
+        },
+      ],
+    }),
+  ],
 })
