f7ced15aa4d97245dac2201c72822c084a5c49e2
bobstrange <bob.1983.g@gmail.com>
2022-04-10 23:39:50 +0900

    feat: bypass props

--- react/test-driven-react/test-driven-carousel/src/components/CarouselButton.test.tsx
+++ react/test-driven-react/test-driven-carousel/src/components/CarouselButton.test.tsx
@@ -1,16 +1,18 @@
-import { describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import { CarouselButton } from './CarouselButton'
 import { render, screen } from '@testing-library/react'
 
 describe('CarouselButton', () => {
+  beforeEach(() => {
+    const text = 'Button Text'
+    render(<CarouselButton>{text}</CarouselButton>)
+  })
+
   it('renders <button />', () => {
-    render(<CarouselButton />)
     expect(screen.getByRole('button')).toBeInTheDocument()
   })
 
   it('renders children component', () => {
-    const text = 'Button Text'
-    render(<CarouselButton>{text}</CarouselButton>)
     expect(screen.getByText('Button Text')).toBeInTheDocument()
   })
 })
--- react/test-driven-react/test-driven-carousel/src/components/CarouselButton.tsx
+++ react/test-driven-react/test-driven-carousel/src/components/CarouselButton.tsx
@@ -1,7 +1,5 @@
 import { FC, PropsWithChildren } from 'react'
 
-export const CarouselButton: FC<PropsWithChildren<Record<string, never>>> = ({
-  children,
-}) => {
-  return <button>{children}</button>
+export const CarouselButton: FC<PropsWithChildren<{}>> = (props) => {
+  return <button {...props} />
 }
