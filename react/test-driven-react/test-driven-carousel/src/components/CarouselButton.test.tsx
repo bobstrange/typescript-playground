b58977b4ecf44febd41d782402dc5fb367d8f618
bobstrange <bob.1983.g@gmail.com>
2022-04-10 23:02:27 +0900

    feat: add fail test

--- react/test-driven-react/test-driven-carousel/src/components/CarouselButton.test.tsx
+++ react/test-driven-react/test-driven-carousel/src/components/CarouselButton.test.tsx
@@ -5,6 +5,12 @@ import { render, screen } from '@testing-library/react'
 describe('CarouselButton', () => {
   it('renders <button />', () => {
     render(<CarouselButton />)
-    expect(screen.getByRole('button')).toBeDefined()
+    expect(screen.getByRole('button')).toBeInTheDocument()
+  })
+
+  it('renders children component', () => {
+    const text = 'Button Text'
+    render(<CarouselButton>{text}</CarouselButton>)
+    expect(screen.getByText('Button Text')).toBeInTheDocument()
   })
 })
