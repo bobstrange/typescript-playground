9ced32dfe5ae7467216037d66a448a5f6fe058be
bobstrange <bob.1983.g@gmail.com>
2022-04-15 23:27:41 +0900

    feat: add TypeScript

---
+++ testing/static-testing-tools/src/typescript-example.ts
@@ -0,0 +1,22 @@
+const add = (a: number, b: number): number => {
+  return a + b
+}
+
+interface User {
+  name: {
+    first: string
+    middle: string
+    last: string
+  }
+}
+
+const getFullName = (user: User): string => {
+  const {
+    name: {first, middle, last},
+  } = user
+
+  return [first, middle, last].filter(Boolean).join(' ')
+}
+
+add(1, 2)
+getFullName({name: {first: 'Joe', middle: 'Bud', last: 'Matthews'}})
