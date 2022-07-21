4f643e00e8ea26cc83110aa5f2eb4bcba2fedd20
bobstrange <bob.1983.g@gmail.com>
2022-07-21 23:55:12 +0900

    feat: create users page

---
+++ nextjs/real-world-nextjs/ssr-rest-api/src/pages/users/index.tsx
@@ -0,0 +1,44 @@
+import { NextPage } from "next"
+import { useEffect, useState } from "react"
+import Link from "next/link"
+import axios from "axios"
+import { User } from "../../types"
+
+const List: React.FC<{ users: User[] }> = ({ users }) => {
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          <Link href={`/users/${user.username}`} passHref>
+            <a>{user.username}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+const Users: NextPage = () => {
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState<User[] | null>(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get<User[]>(
+        "https://jsonplaceholder.typicode.com/users"
+      )
+      setLoading(false)
+      setData(res.data)
+    }
+    fetchData()
+  }, [])
+
+  return (
+    <div>
+      {loading && <div>Loading users...</div>}
+      {data && <List users={data} />}
+    </div>
+  )
+}
+
+export default Users
