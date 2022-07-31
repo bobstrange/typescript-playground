ba32317f6ad6478a573c6bd93b8f6b0a915f80cd
bobstrange <bob.1983.g@gmail.com>
2022-08-01 00:55:51 +0900

    feat: darkmode

--- nextjs/real-world-nextjs/employee-directory-with-chakra-ui/pages/_app.tsx
+++ nextjs/real-world-nextjs/employee-directory-with-chakra-ui/pages/_app.tsx
@@ -1,9 +1,20 @@
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
 import type { AppProps } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const customTheme = extendTheme({
+    colors: {
+      brand: {
+        100: "#ffebee",
+        200: "#e57373",
+        300: "#f44336",
+        400: "#e53935",
+      },
+    },
+  })
+
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={customTheme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
