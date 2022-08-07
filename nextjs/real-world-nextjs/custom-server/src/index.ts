24f78587227b6fd59e78b2692cd54aa0ed637bee
bobstrange <bob.1983.g@gmail.com>
2022-08-07 23:53:25 +0900

    chore: add index

---
+++ nextjs/real-world-nextjs/custom-server/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express"
+import { parse } from "url"
+import next from "next"
+
+const dev = process.env.NODE_ENV !== "production"
+const app = next({ dev })
+
+async function main() {
+  try {
+    await app.prepare()
+    const handle = app.getRequestHandler()
+    const server = express()
+
+    server.get("*", (req, res) => {
+      const url = parse(req.url, true)
+      handle(req, res, url)
+    })
+    server.listen(3000, () => {
+      console.log("listening on port 3000")
+    })
+  } catch (err) {
+    console.log(err.stack)
+  }
+}
+
+main()
