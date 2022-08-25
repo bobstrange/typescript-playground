ed8aff6d470d13c9a5e373b44c3e52863e12eb22
bobstrange <bob.1983.g@gmail.com>
2022-08-26 00:40:33 +0900

    chore: tidy

--- amplify/amplify-js-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "amplifyplayground": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
