5e66bebda35c1a6d6a7595a7e00def2e41c08b4b
bobstrange <bob.1983.g@gmail.com>
2022-12-07 23:45:09 +0900

    chore: debug html

--- debugging/src/index.ts
+++ debugging/src/index.ts
@@ -1,6 +1,3 @@
-import { divide, multiply, subtract, sum } from "./calc"
+const message = "Hello world"
 
-console.log("sum(100, 200) = ", sum(100, 200))
-console.log("subtract(100, 200) = ", subtract(100, 200))
-console.log("multiply(100, 200) = ", multiply(100, 200))
-console.log("divide(100, 200) = ", divide(100, 200))
+document.body.innerHTML = message
