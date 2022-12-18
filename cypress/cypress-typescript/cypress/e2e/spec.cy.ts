71848970595bcbe458c8d1cfe935fe8daf382b45
bobstrange <bob.1983.g@gmail.com>
2022-12-19 00:16:23 +0900

    chore: add app

--- cypress/cypress-typescript/cypress/e2e/spec.cy.ts
+++ cypress/cypress-typescript/cypress/e2e/spec.cy.ts
@@ -1,5 +1,9 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+import { expect } from "chai"
+
+describe("empty spec", () => {
+  it("passes", () => {
+    cy.visit("https://docs.cypress.io/guides/overview/why-cypress")
+    cy.get("h1").should("be.visible", "Why Cypress?")
+    cy.get("#docsearch").type("TypeScript")
   })
 })
