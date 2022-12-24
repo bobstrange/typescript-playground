4c1ebea0382430e8aa2776c272bf474bb7e07b1f
bobstrange <bob.1983.g@gmail.com>
2022-12-24 23:06:05 +0900

    chore: remove unused spec

--- cypress/cypress-typescript/cypress/e2e/spec.cy.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from "chai"
-
-describe("empty spec", () => {
-  it("passes", () => {
-    cy.visit("https://docs.cypress.io/guides/overview/why-cypress")
-    cy.get("h1").should("be.visible", "Why Cypress?")
-    cy.get("#docsearch").type("TypeScript")
-  })
-})
