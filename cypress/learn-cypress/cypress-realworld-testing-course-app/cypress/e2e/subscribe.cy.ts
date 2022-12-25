2ddadd6d3a95a2db34964a4897b9366124d9ea90
bobstrange <bob.1983.g@gmail.com>
2022-12-25 23:34:51 +0900

    chore: add invalid email test

--- cypress/learn-cypress/cypress-realworld-testing-course-app/cypress/e2e/subscribe.cy.ts
+++ cypress/learn-cypress/cypress-realworld-testing-course-app/cypress/e2e/subscribe.cy.ts
@@ -3,10 +3,17 @@ describe("Newsletter subscribe form", () => {
     cy.visit("http://localhost:3000")
   })
 
-  it.only("allows users to subscribe to email list", () => {
+  it("allows users to subscribe to email list", () => {
     const email = "john-doe@aaa.bbb.ccc.dddd.com"
     cy.dataTest("email-input").type(email)
     cy.dataTest("submit-button").click()
     cy.dataTest("success-message").should("exist").contains(`${email}`)
+  })
+
+  it("does NOT allow a invalid email", () => {
+    const invalidEmail = "john-doe"
+    cy.dataTest("email-input").type(invalidEmail)
+    cy.dataTest("submit-button").click()
+    cy.dataTest("success-message").should("not.exist")
   })
 })
