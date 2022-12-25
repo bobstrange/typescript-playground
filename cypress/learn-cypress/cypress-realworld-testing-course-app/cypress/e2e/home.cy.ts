da057995ab5bf5b77703ba963066784b1aaa8106
bobstrange <bob.1983.g@gmail.com>
2022-12-25 23:51:18 +0900

    chore: verify page transition

--- cypress/learn-cypress/cypress-realworld-testing-course-app/cypress/e2e/home.cy.ts
+++ cypress/learn-cypress/cypress-realworld-testing-course-app/cypress/e2e/home.cy.ts
@@ -3,15 +3,24 @@ describe("home page", () => {
     cy.visit("http://localhost:3000")
   })
 
-  it("the h1 contains the correct test", () => {
-    cy.dataTest("hero-heading").contains(
-      "Testing Next.js Applications with Cypress"
-    )
+  context("hero section", () => {
+    it("the h1 contains the correct test", () => {
+      cy.dataTest("hero-heading").contains(
+        "Testing Next.js Applications with Cypress"
+      )
+    })
+
+    it("the features on the homepage are correct", () => {
+      cy.get("dt").eq(0).contains("4 Courses")
+      cy.get("dt").eq(1).contains("25+ Lessons")
+      cy.get("dt").eq(2).contains("Free and Open Source")
+    })
   })
 
-  it("the features on the homepage are correct", () => {
-    cy.get("dt").eq(0).contains("4 Courses")
-    cy.get("dt").eq(1).contains("25+ Lessons")
-    cy.get("dt").eq(2).contains("Free and Open Source")
+  context("courses section", () => {
+    it("Course: Testing Your First Next.js Application", () => {
+      cy.dataTest("course-0").find("a").eq(3).click()
+      cy.location("pathname").should("eq", "/testing-your-first-application")
+    })
   })
 })
