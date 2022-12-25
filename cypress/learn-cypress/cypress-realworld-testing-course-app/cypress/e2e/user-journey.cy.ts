23b6db31607b6b6b4756e17ffdc893f06827ae95
bobstrange <bob.1983.g@gmail.com>
2022-12-26 00:12:54 +0900

    chore: done user journey test

--- cypress/learn-cypress/cypress-realworld-testing-course-app/cypress/e2e/user-journey.cy.ts
+++ cypress/learn-cypress/cypress-realworld-testing-course-app/cypress/e2e/user-journey.cy.ts
@@ -5,5 +5,30 @@ describe("User journey", () => {
     cy.location("pathname").should("eq", "/testing-your-first-application")
 
     cy.dataTest("next-lesson-button").click()
+    cy.location("pathname").should(
+      "eq",
+      "/testing-your-first-application/app-install-and-overview"
+    )
+
+    cy.dataTest("challenge-answer-0").click()
+    cy.dataTest("next-lesson-button").click()
+
+    cy.location("pathname").should(
+      "eq",
+      "/testing-your-first-application/installing-cypress-and-writing-our-first-test"
+    )
+
+    cy.dataTest("challenge-answer-0").click()
+    cy.dataTest("next-lesson-button").click()
+
+    cy.location("pathname").should(
+      "eq",
+      "/testing-your-first-application/setting-up-data-before-each-test"
+    )
+
+    cy.dataTest("challenge-answer-0").click()
+    cy.dataTest("next-lesson-button").click()
+
+    cy.location("pathname").should("eq", "/")
   })
 })
