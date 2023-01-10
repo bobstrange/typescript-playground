a271e9f3c149b81c2563697f459ad49acd16948b
bobstrange <bob.1983.g@gmail.com>
2023-01-10 22:37:03 +0900

    chore: add @deno-types to import types

--- deno/hello-deno/src/06_npm_packages.ts
+++ deno/hello-deno/src/06_npm_packages.ts
@@ -1,7 +1,18 @@
 import chalk from "npm:chalk@5";
 
+// @deno-types="npm:@types/express@4"
+import express from "npm:express@4";
+
 console.log(chalk.green("Hi there"));
 
+const app = express();
+app.get("/", (_, res) => {
+  res.send("Hi there");
+});
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
+
 /**
- * deno run --allow-env src/06_npm_packages.ts
+ * deno run --allow-env --allow-read --allow-net src/06_npm_packages.ts
  */
