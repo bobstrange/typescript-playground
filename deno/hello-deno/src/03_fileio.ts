c2a6097ec1bc436368af7671a491f699a3c868c6
bobstrange <bob.1983.g@gmail.com>
2023-01-10 23:33:59 +0900

    chore: write json to file

--- deno/hello-deno/src/03_fileio.ts
+++ deno/hello-deno/src/03_fileio.ts
@@ -1,9 +1,5 @@
 import { copy } from "https://deno.land/std@0.171.0/streams/conversion.ts";
-import { fromFileUrl } from "https://deno.land/std@0.171.0/path/mod.ts";
-
-/**
- * deno run --allow-read hello-deno/src/03_fileio.ts /etc/hosts
- */
+import { join, fromFileUrl } from "https://deno.land/std@0.171.0/path/mod.ts";
 
 const filenames = Deno.args;
 
@@ -16,3 +12,21 @@ filenames.forEach(async (filename) => {
 
 const text = await Deno.readTextFile(fromFileUrl(import.meta.url));
 console.log("text: ", text);
+
+const data = [
+  {
+    name: "John",
+    age: 20,
+  },
+  {
+    name: "Jane",
+    age: 21,
+  },
+];
+
+const dataPath = join(fromFileUrl(import.meta.url), "../../tmp/data.json");
+await Deno.writeTextFile(dataPath, JSON.stringify(data));
+
+/**
+ * deno run --allow-read --allow-write src/03_fileio.ts /etc/hosts
+ */
