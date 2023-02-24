03af09878e6726f34a3cb6c64600dbec2e951df5
bobstrange <bob.1983.g@gmail.com>
2023-02-25 01:09:35 +0900

    chore: respond to hello

--- bolt-app/app.js
+++ bolt-app/app.js
@@ -1,14 +1,23 @@
-const { App } = require("@slack/bolt");
+const { App } = require('@slack/bolt')
 
-// ボットトークンと Signing Secret を使ってアプリを初期化します
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-});
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  // ソケットモードではポートをリッスンしませんが、アプリを OAuth フローに対応させる場合、
+  // 何らかのポートをリッスンする必要があります
+  port: process.env.PORT || 3000,
+})
 
-(async () => {
+// "hello" を含むメッセージをリッスンします
+app.message('hello', async ({ message, say }) => {
+  // イベントがトリガーされたチャンネルに say() でメッセージを送信します
+  await say(`Hey there <@${message.user}>!`)
+})
+;(async () => {
   // アプリを起動します
-  await app.start(process.env.PORT || 3000);
+  await app.start()
 
-  console.log("⚡️ Bolt app is running!");
-})();
+  console.log('⚡️ Bolt app is running!')
+})()
