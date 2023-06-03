87e6494b77ffb2dd2d11e96e098f4ec171f0d142
bobstrange <bob.1983.g@gmail.com>
2023-06-03 23:58:20 +0900

    chore: test if a callback is triggerd when the button is clicked

--- frontend-testing/unit-test/src/components/Form.test.tsx
+++ frontend-testing/unit-test/src/components/Form.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '../utils/test-utils'
+import { fireEvent, render, screen } from '../utils/test-utils'
 import { Form } from './Form'
 
 test('name is shown', () => {
@@ -14,4 +14,11 @@ test('button is shown', () => {
 test('heading is shown', () => {
   render(<Form name="John Doe" />)
   expect(screen.getByRole('heading')).toHaveTextContent('Account Info')
+})
+
+test('event handler is called when button is clicked', () => {
+  const mockFn = vi.fn()
+  render(<Form name="John Doe" onSubmit={mockFn} />)
+  fireEvent.click(screen.getByRole('button'))
+  expect(mockFn).toHaveBeenCalled()
 })
