5766c4acf6a8db95baf5ec27b7e42be5da2ccbb0
bobstrange <bob.1983.g@gmail.com>
2023-06-03 23:54:13 +0900

    chore: test button and heading

--- frontend-testing/unit-test/src/components/Form.test.tsx
+++ frontend-testing/unit-test/src/components/Form.test.tsx
@@ -3,6 +3,15 @@ import { Form } from './Form'
 
 test('name is shown', () => {
   render(<Form name="John Doe" />)
-
   expect(screen.getByText(/John Doe/i)).toBeInTheDocument()
+})
+
+test('button is shown', () => {
+  render(<Form name="John Doe" />)
+  expect(screen.getByRole('button')).toBeInTheDocument()
+})
+
+test('heading is shown', () => {
+  render(<Form name="John Doe" />)
+  expect(screen.getByRole('heading')).toHaveTextContent('Account Info')
 })
