ba6084b773df30aeceed5022a6053e25d9aabf88
bobstrange <bob.1983.g@gmail.com>
2023-06-04 23:43:41 +0900

    chore: add test for checkbox

--- frontend-testing/unit-test/src/components/Agreement.test.tsx
+++ frontend-testing/unit-test/src/components/Agreement.test.tsx
@@ -1,7 +1,19 @@
-import { screen, render } from '../utils/test-utils'
+import { screen, render, fireEvent } from '../utils/test-utils'
 import { Agreement } from './Agreement'
 
 test('fieldset accessible name quotes legend', () => {
   render(<Agreement />)
   expect(screen.getByRole('group', { name: 'ToS Agreement' })).toBeInTheDocument()
+})
+
+test('checkbox is not checked by default', () => {
+  render(<Agreement />)
+  expect(screen.getByRole('checkbox')).not.toBeChecked()
+})
+
+test('checkbox is checked when clicked', () => {
+  render(<Agreement />)
+  const checkbox = screen.getByRole('checkbox')
+  fireEvent.click(checkbox)
+  expect(checkbox).toBeChecked()
 })
