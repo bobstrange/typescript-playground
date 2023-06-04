d7c1f464c12fcb89bb163e6ebaad6ff990b203b0
bobstrange <bob.1983.g@gmail.com>
2023-06-04 23:02:58 +0900

    chore: use within

--- frontend-testing/unit-test/src/components/ArticleList.test.tsx
+++ frontend-testing/unit-test/src/components/ArticleList.test.tsx
@@ -1,10 +1,11 @@
-import { render, screen } from '../utils/test-utils'
+import { render, screen, within } from '../utils/test-utils'
 import { ArticleList } from './ArticleList'
 import { articles } from '../test/fixture'
 
 test('show articles', () => {
   render(<ArticleList items={articles} />)
 
-  expect(screen.getByRole('list')).toBeInTheDocument()
-  expect(screen.getAllByRole('listitem')).toHaveLength(articles.length)
+  const list = screen.getByRole('list')
+  expect(list).toBeInTheDocument()
+  expect(within(list).getAllByRole('listitem')).toHaveLength(articles.length)
 })
