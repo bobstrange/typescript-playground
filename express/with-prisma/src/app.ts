532932e0463d63fdb7c08a8b5f5d9d56274dbe5e
bobstrange <bob.1983.g@gmail.com>
2023-06-12 23:12:39 +0900

    chore: add cors middleware

--- express/with-prisma/src/app.ts
+++ express/with-prisma/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import morgan from "morgan"
+import cors from "cors"
 
 import { router } from "./router"
 
@@ -7,6 +8,12 @@ const app = express()
 app.use(morgan("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+app.use((req, res, next) => {
+  req.something_secret = "doggy"
+  next()
+})
 
 app.get("/", (req, res) => {
   console.log("Hello")
