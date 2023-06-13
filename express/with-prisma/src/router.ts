0d09304b46f2026a5298cc01efdeccb626806f3e
bobstrange <bob.1983.g@gmail.com>
2023-06-14 00:17:11 +0900

    chore: add name validator

--- express/with-prisma/src/router.ts
+++ express/with-prisma/src/router.ts
@@ -1,12 +1,22 @@
 import { Router } from "express"
+import { query, validationResult } from "express-validator"
 
 const router = Router()
+
+const nameValidateChain = () => query("name").isString()
 
 router.get("/products", (req, res) => {
   res.json({ message: req.something_secret })
 })
 router.get("/products/:id", () => {})
-router.put("/products/:id", () => {})
+router.put("/products/:id", nameValidateChain(), (req, res) => {
+  const errors = validationResult(req)
+
+  console.log(errors)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+})
 router.post("/products", () => {})
 router.delete("/products/:id", () => {})
 
