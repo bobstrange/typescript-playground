de3c5fb4946c57f9dd775707d231f7049207d48c
bobstrange <bob.1983.g@gmail.com>
2023-06-15 23:57:47 +0900

    chore: use isIn validator

--- express/with-prisma/src/router.ts
+++ express/with-prisma/src/router.ts
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { query } from "express-validator"
+import { query, body, oneOf } from "express-validator"
 import { handleInputError } from "./modules/middleware"
 
 const router = Router()
@@ -11,12 +11,19 @@ router.get("/products", (req, res) => {
 })
 router.get("/products/:id", () => {})
 router.put("/products/:id", nameValidateChain(), handleInputError, (req, res) => {})
-router.post("/products", () => {})
+router.post("/products", nameValidateChain(), handleInputError, (req, res) => {})
 router.delete("/products/:id", () => {})
 
 router.get("/updates", () => {})
 router.get("/updates/:id", () => {})
-router.put("/updates/:id", () => {})
+router.put(
+  "/updates/:id",
+  body("title").optional(),
+  body("body").optional(),
+  body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]).optional(
+  body("version").optional(),
+  () => {}
+)
 router.post("/updates", () => {})
 router.delete("/updates/:id", () => {})
 
