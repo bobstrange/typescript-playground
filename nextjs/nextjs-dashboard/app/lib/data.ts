a9298a8de4fa7c937a7fcdfcb50c9823029f2935
bobstrange <bob.1983.g@gmail.com>
2023-12-01 00:39:35 +0900

    feat: show invoices

--- nextjs/nextjs-dashboard/app/lib/data.ts
+++ nextjs/nextjs-dashboard/app/lib/data.ts
@@ -39,17 +39,21 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    const data = await sql<LatestInvoiceRaw>`
+    const client = await pool.connect()
+
+    const data = await client.query<LatestInvoiceRaw>(`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
-      LIMIT 5`
+      LIMIT 5`)
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }))
+    client.release()
+
     return latestInvoices
   } catch (error) {
     console.error('Database Error:', error)
