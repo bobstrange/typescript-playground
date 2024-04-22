10868acbf833f3cbcb9490a63b8e9441cfabe649
bobstrange <bob.1983.g@gmail.com>
2024-04-22 21:14:28 +0900

    chore: parse awsdo result

--- deno/update-env/deps.ts
+++ deno/update-env/deps.ts
@@ -1,2 +1,3 @@
 export * from "https://deno.land/std@0.223.0/dotenv/mod.ts";
+export * from "https://deno.land/std@0.223.0/assert/mod.ts";
 export { default as $ } from "https://deno.land/x/dax@0.39.2/mod.ts";
--- deno/update-env/src/main_test.ts
+++ deno/update-env/src/main_test.ts
@@ -1,4 +1,4 @@
-import { $, load } from "../deps.ts";
+import { $, load, assertEquals } from "../deps.ts";
 
 Deno.test("Test $ works", async () => {
   await $`echo 5`;
@@ -7,6 +7,54 @@ Deno.test("Test $ works", async () => {
 Deno.test("Run awsdo command and return the output", async () => {
   const env = await load();
   const profile = env["AWS_PROFILE"];
+
   const result = await $`awsdo --profile ${profile} --duration 12hours`.text();
   console.log(result);
+});
+
+function parseAwsdoResult(input: string) {
+  const lines = input.split("\n");
+
+  const awsRegion = lines
+    .filter((line) => line.includes("AWS_REGION"))
+    .at(0)
+    ?.replace("export AWS_REGION=", "");
+  const awsAccessKeyId = lines
+    .filter((line) => line.includes("AWS_ACCESS_KEY_ID"))
+    .at(0)
+    ?.replace("export AWS_ACCESS_KEY_ID=", "");
+  const awsSecreteAccessKey = lines
+    .filter((line) => line.includes("AWS_SECRET_ACCESS_KEY"))
+    .at(0)
+    ?.replace("export AWS_SECRET_ACCESS_KEY=", "");
+  const awsSessionToken = lines
+    .filter((line) => line.includes("AWS_SESSION_TOKEN"))
+    .at(0)
+    ?.replace("export AWS_SESSION_TOKEN=", "");
+
+  return {
+    awsRegion,
+    awsAccessKeyId,
+    awsSecreteAccessKey,
+    awsSessionToken,
+  };
+}
+
+Deno.test("parse awsdo result", async () => {
+  const result = `export AWS_REGION=ap-northeast-1
+export AWS_ACCESS_KEY_ID=ASIAxxxxxx
+export AWS_SECRET_ACCESS_KEY=secret_access_key
+export AWS_SESSION_TOKEN=new_session_token
+`;
+  const { awsRegion, awsAccessKeyId, awsSecreteAccessKey, awsSessionToken } =
+    parseAwsdoResult(result);
+  assertEquals(awsRegion, "ap-northeast-1");
+  assertEquals(awsAccessKeyId, "ASIAxxxxxx");
+  assertEquals(awsSecreteAccessKey, "secret_access_key");
+  assertEquals(awsSessionToken, "new_session_token");
+});
+
+Deno.test("read data from .envrc file", async () => {
+  const text = await Deno.readTextFile("src/.envrc_test");
+  console.log(text);
 });
